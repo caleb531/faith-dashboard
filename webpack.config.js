85071273cd3542d7186339e1a5c4a63eeef745bc
Caleb Evans <caleb@calebevans.me>
2021-11-07 10:00:42 -0800

    Properly load React into application

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,13 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader"
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            ["@babel/preset-react", {"runtime": "automatic"}]
+          ]
+        }
       }
     ]
   },
