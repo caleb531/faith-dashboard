f57398ca482d4900b5cc179ca5e5551e4aec3a2d
Caleb Evans <caleb@calebevans.me>
2021-11-07 10:18:52 -0800

    Rewrite App component

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,21 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Faith Dashboard</h1>
+      </div>
+    );
+  }
+
 }
 
 export default App;
