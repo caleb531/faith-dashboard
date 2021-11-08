eef1d0ebaba42a9318554e009f8b6e7811768880
Caleb Evans <caleb@calebevans.me>
2021-11-07 16:36:19 -0800

    Clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,27 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/scripts/index.jsx",
-  mode: "development",
+  entry: './src/scripts/index.jsx',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
           presets: [
-            "@babel/preset-env",
-            ["@babel/preset-react", {"runtime": "automatic"}]
+            '@babel/preset-env',
+            ['@babel/preset-react', { 'runtime': 'automatic' }]
           ]
         }
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
   }
 };
