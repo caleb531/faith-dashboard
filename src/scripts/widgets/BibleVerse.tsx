9604f0fa8dff8932b5271877851c47a65fffde55
Caleb Evans <caleb@calebevans.me>
2021-11-13 15:20:26 -0800

    Allow verse to be set more than once

--- src/scripts/widgets/BibleVerse.tsx
+++ src/scripts/widgets/BibleVerse.tsx
@@ -8,6 +8,7 @@ function BibleVerse({ widget }: { widget: WidgetState }) {
   const { app, dispatchApp } = useContext(AppContext);
   const [ verseContent, setVerseContent ] = useState(null);
   const [ verseQuery, setVerseQuery ] = useState(widget.data.verseQuery);
+  const [ isFetchingVerse, setIsFetchingVerse ] = useState(false);
   const searchInputRef: {current: HTMLInputElement} = useRef();
 
   // Call the ESV API through a proxy endpoint because the ESV API does not
@@ -18,12 +19,16 @@ function BibleVerse({ widget }: { widget: WidgetState }) {
     if (!query) {
       return;
     }
+    setIsFetchingVerse(true);
     fetch(`${API_URL}?q=${encodeURIComponent(query)}`)
       .then((verseResponse) => verseResponse.json())
       .then((verseData) => {
         console.log('verseData', verseData);
+        setIsFetchingVerse(false);
         if (verseData.passages) {
-          setVerseContent(verseData.passages.join(''));
+          setVerseContent(verseData.passages);
+        } else {
+          setVerseContent(null);
         }
       });
   }
@@ -46,11 +51,11 @@ function BibleVerse({ widget }: { widget: WidgetState }) {
 
   return (
     <section className="bible-verse">
-      {verseQuery && !verseContent ? (
+      {verseQuery && isFetchingVerse ? (
         <div className="widget-loading">Loading...</div>
-      ) : verseQuery && verseContent ? (
+      ) : verseQuery && verseContent && verseContent.length ? (
           <div className="bible-verse-content">
-            {HtmlReactParser(verseContent)}
+            {HtmlReactParser(verseContent.join(''))}
           </div>
         ) : (
             <>
@@ -65,6 +70,9 @@ function BibleVerse({ widget }: { widget: WidgetState }) {
                 defaultValue={verseQuery}
                 ref={searchInputRef} />
               <button className="bible-verse-picker-submit">Submit</button>
+              {verseQuery && !(verseContent && verseContent.length) ? (
+                <p className="bible-verse-no-results">No Results Found</p>
+              ) : null}
               </form>
             </>
           )
