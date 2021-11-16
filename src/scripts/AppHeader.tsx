a3346fa7a36cd7c139b22ca1fb50e13a9b6e96be
Caleb Evans <caleb@calebevans.me>
2021-11-16 14:29:22 -0800

    Use <header> tag for app header

--- src/scripts/AppHeader.tsx
+++ src/scripts/AppHeader.tsx
@@ -4,10 +4,10 @@ import ThemeSwitcher from './ThemeSwitcher';
 function AppHeader() {
 
   return (
-    <div className="app-header">
+    <header className="app-header">
       <h1 className="app-header-title">Faith Dashboard</h1>
       <ThemeSwitcher />
-    </div>
+    </header>
   );
 
 }
