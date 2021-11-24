ca053a200e2e3cd4b32b4a51c49ca5ce29ca7210
Caleb Evans <caleb@calebevans.me>
2021-11-23 21:28:46 -0800

    Make widget controls more accessible

--- src/scripts/Widget.tsx
+++ src/scripts/Widget.tsx
@@ -54,25 +54,29 @@ function Widget({ widget, provided }: { widget: WidgetState, provided: Draggable
   return (
     <article className={`widget widget-type-${widget.type} ${state.isSettingsOpen ? 'widget-settings-open' : ''}`} ref={provided.innerRef} {...provided.draggableProps} style={widgetStyles} onMouseUp={handleResize}>
       <div className="widget-controls widget-controls-left">
-        <img
-          src="icons/drag-handle-light.svg"
-          alt="Drag Widget"
-          draggable="false"
-          className="widget-drag-handle widget-control" {...provided.dragHandleProps} />
+        <div className="widget-drag-handle widget-control" {...provided.dragHandleProps}>
+          <img
+            src="icons/drag-handle-light.svg"
+            alt="Drag Widget"
+            draggable="false"
+            className="widget-drag-handle-icon widget-control-icon" />
+        </div>
       </div>
       <div className="widget-controls widget-controls-right">
-        <img
-          src="icons/remove-circle-light.svg"
-          alt="Remove Widget"
-          className="widget-remove-control widget-control"
-          draggable="false"
-          onClick={() => removeWidget()} />
-        <img
-          src="icons/settings-light.svg"
-          alt="Toggle Settings"
-          draggable="false"
-          className="widget-settings-toggle widget-control"
-          onClick={(event) => dispatch({ type: 'toggleSettings' })} />
+        <button className="widget-remove-control widget-control" onClick={() => removeWidget()}>
+          <img
+            src="icons/remove-circle-light.svg"
+            alt="Remove Widget"
+            className="widget-remove-control-icon widget-control-icon"
+            draggable="false" />
+        </button>
+        <button className="widget-settings-toggle widget-control" onClick={(event) => dispatch({ type: 'toggleSettings' })}>
+          <img
+            src="icons/settings-light.svg"
+            alt="Toggle Settings"
+            draggable="false"
+            className="widget-settings-toggle-icon widget-control-icon" />
+        </button>
       </div>
       <WidgetContents widget={state} widgetData={state.data} dispatchWidget={dispatch} />
     </article>
