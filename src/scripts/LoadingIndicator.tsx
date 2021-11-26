ddf983daea29d7a672f402d016d57ef008f2d13e
Caleb Evans <caleb@calebevans.me>
2021-11-26 10:23:58 -0800

    Replace "Loading..." text with animated spinner

--- src/scripts/LoadingIndicator.tsx
+++ src/scripts/LoadingIndicator.tsx
@@ -4,7 +4,9 @@ function AppLoadingIndicator() {
 
   return (
     <div className="loading-indicator">
-      Loading...
+      <svg viewBox="0 0 32 32" className="loading-indicator-icon">
+        <path d="M 16,2 A 8,8 0,0,1 16,30" />
+      </svg>
     </div>
   );
 
