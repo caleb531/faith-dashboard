7a9ce67945c368a5412053104e04db852a6365f2
Caleb Evans <caleb@calebevans.me>
2021-11-25 18:22:25 -0800

    Add personal credit next to dedication
    So people at least know who made the app.

--- src/scripts/AppFooter.tsx
+++ src/scripts/AppFooter.tsx
@@ -4,7 +4,7 @@ function AppFooter() {
 
   return (
     <footer className="app-footer">
-      <span className="footer-dedication">Dedicated to Christ our Lord</span>
+      <span className="footer-dedication">By Caleb Evans. Dedicated to Christ our Lord</span>
       &nbsp;&middot;&nbsp;
       <a href="copyright.html">Copyright</a>
     </footer>
