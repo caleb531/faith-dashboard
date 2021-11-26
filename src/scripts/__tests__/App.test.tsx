8f594643095193a677fec9bf2bd040f23c1991c9
Caleb Evans <caleb@calebevans.me>
2021-11-25 17:19:57 -0800

    Add additional widget tests

--- src/scripts/__tests__/App.test.tsx
+++ src/scripts/__tests__/App.test.tsx
@@ -73,7 +73,53 @@ describe('App Component', function () {
           destinationColumn: 2
         }
       }
-    )).toEqual({ theme: AppTheme.teal, widgets: [widgets[0], widgets[2], widgets[0], widgets[2]] });
+    )).toEqual({ theme: AppTheme.teal, widgets: [widgets[1], { ...widgets[0], column: 2 }, widgets[2], widgets[3]] });
+  });
+
+  it('should move widget from column 2 to column 1', function () {
+    const widgets = [
+      createWidget({ column: 1 }),
+      createWidget({ column: 1 }),
+      createWidget({ column: 2 }),
+      createWidget({ column: 3 })
+    ] as WidgetState[];
+    const app = { theme: AppTheme.teal, widgets };
+    expect(reducer(
+      app,
+      {
+        type: 'moveWidget',
+        payload: {
+          widgetToMove: widgets[2],
+          sourceIndex: 2,
+          sourceColumn: 2,
+          destinationIndex: 1,
+          destinationColumn: 1
+        }
+      }
+    )).toEqual({ theme: AppTheme.teal, widgets: [widgets[0], { ...widgets[2], column: 1 }, widgets[1], widgets[3]] });
+  });
+
+  it('should keep widgets array in column order after move', function () {
+    const widgets = [
+      createWidget({ column: 1 }),
+      createWidget({ column: 1 }),
+      createWidget({ column: 1 }),
+      createWidget({ column: 3 })
+    ] as WidgetState[];
+    const app = { theme: AppTheme.teal, widgets };
+    expect(reducer(
+      app,
+      {
+        type: 'moveWidget',
+        payload: {
+          widgetToMove: widgets[3],
+          sourceIndex: 3,
+          sourceColumn: 3,
+          destinationIndex: 0,
+          destinationColumn: 2
+        }
+      }
+    )).toEqual({ theme: AppTheme.teal, widgets: [widgets[0], widgets[1], widgets[2], { ...widgets[3], column: 2 }] });
   });
 
 });
