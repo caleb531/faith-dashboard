48fabb3e1092cecdae3a1acb48e27e43edca6724
Caleb Evans <caleb@calebevans.me>
2021-11-28 20:59:30 -0800

    Set initial Settings state for default widgets

--- src/data/appStateDefault.ts
+++ src/data/appStateDefault.ts
@@ -7,25 +7,29 @@ export default {
       id: '2c342850-2237-4dab-8b08-b10cae7c7a4e',
       type: WidgetTypeId.BibleVerse,
       column: 1,
-      data: {}
+      data: {},
+      isSettingsOpen: true
     },
     {
       id: '50546223-76c8-4643-a402-87c4cf213849',
       type: WidgetTypeId.Note,
       column: 1,
-      data: {}
+      data: {},
+      isSettingsOpen: false
     },
     {
       id: '4deca405-3e4e-4baa-94c8-82ebf5bcbcde',
       type: WidgetTypeId.BibleVerse,
       column: 2,
-      data: {}
+      data: {},
+      isSettingsOpen: true
     },
     {
       id: '0f0923aa-6ba1-4958-9168-41a5085a57c2',
       type: WidgetTypeId.Note,
       column: 3,
-      data: {}
+      data: {},
+      isSettingsOpen: false
     }
   ]
 };
