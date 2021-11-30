583bdaa672c6b6f8c55b345dc40a9c5eb02ef540
Caleb Evans <caleb@calebevans.me>
2021-11-29 18:06:55 -0800

    Revert to working Workbox v5 service worker
    I'm more familiar witht the v5 API and I'm not in a rush to implement
    Workbox v6.

--- src/scripts/service-worker.js
+++ src/scripts/service-worker.js
@@ -1,20 +1,20 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // The following call will be populated automatically with the precached file
 // data during the build step
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 // Cache Google fonts
-workbox.routing.registerRoute(new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'), workbox.strategies.cacheFirst({
+workbox.routing.registerRoute(new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'), new workbox.strategies.CacheFirst({
   cacheName: 'google-fonts',
   plugins: [
     // When the cap is reached, the oldest entries are purged
-    new workbox.expiration.Plugin({
+    new workbox.expiration.ExpirationPlugin({
       maxEntries: 30
     }),
     // The Google Fonts CSS response is an opaque (non-CORS) response with a
     // status code of 0, so we need to enable caching for that type of response
-    new workbox.cacheableResponse.Plugin({
+    new workbox.cacheableResponse.CacheableResponsePlugin({
       statuses: [0, 200]
     })
   ]
@@ -22,11 +22,11 @@ workbox.routing.registerRoute(new RegExp('^https://fonts.(?:googleapis|gstatic).
 
 // When an update to the service worker is detected, the front end will request
 // that the service worker be updated immediately; listen for that request here
-self.addEventListener('message', (event) => {
-  if (!event.data) {
+self.addEventListener('message', (messageEvent) => {
+  if (!messageEvent.data) {
     return;
   }
-  if (event.data.updateManagerEvent === 'update') {
+  if (messageEvent.data && messageEvent.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
