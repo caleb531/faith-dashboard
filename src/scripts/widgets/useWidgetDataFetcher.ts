91b324aeb2dbf0ad219441d7ee570034b85ea3c8
Caleb Evans <caleb@calebevans.me>
2021-12-02 20:40:39 -0800

    Define function signatures for useWidgetDataFetcher() parameters

--- src/scripts/widgets/useWidgetDataFetcher.ts
+++ src/scripts/widgets/useWidgetDataFetcher.ts
@@ -1,5 +1,6 @@
 import React, { useContext, useRef, useEffect } from 'react';
 import { WidgetState } from '../types';
+import { JSONSerializable } from '../types.d';
 
 // The useWidgetDataFetcher() hook is a powerful hook that allows you to fetch
 // arbitrary data from an API endpoint given some user-supplied request query
@@ -13,41 +14,41 @@ export default function useWidgetDataFetcher({ widget, dispatch, shouldFetch, re
   // A boolean function that should return true if the widget should fetch on
   // this render, and false otherwise; normally, this condition should be if
   // the request data is populated, and if there is no cached content
-  shouldFetch: Function,
+  shouldFetch: () => any,
   // The request data to be used in building the API request; this will
   // probably be a user-entered query string of some sort
-  requestData: string | object,
+  requestData: string,
   // Receives the user-entered query as its only argument, and
   // should set query on the widget state (e.g. call the dispatch() function
   // returned by useWidgetShell(), or even a setter function from useState(),
   // and provide the user-entered query string as the payload); this callback
   // receives the user-entered query as its only argument
-  setRequestData: Function,
+  setRequestData: (requestData: string) => void,
   // Receives the request data as input, and returns a string representing the
   // full URL of the API endpoint to call; this
-  getApiUrl: Function,
+  getApiUrl: (requestData: string) => string,
   // Parses the response from the designated API (which is assumed to be a JSON
   // response) by performing any property access to get at the objects or data
   // you actually want to store (or to simplify the object structure for later
   // use)
-  parseResponse: Function,
+  parseResponse: (data: JSONSerializable) => JSONSerializable,
   // A boolean function that should return true if the result of
   // parseResponse() has content to display, or false if (in which case the No
   // Results message is triggered)
-  hasResults: Function,
+  hasResults: (data: JSONSerializable) => any,
   // If hasResults() returns true, this callback runs with the return value of
   // parseResponse() as its only argument; you will probably want to call a
   // dispatch() function or useState() setter within this callback to persist
   // the response data to the widget state
-  onSuccess: Function,
+  onSuccess: (data: JSONSerializable) => void,
   // Accepts the return value of parseResponse() as its only argument, and
   // should return a string representing the message to show if hasResults()
   // returns false
-  getNoResultsMessage: Function,
+  getNoResultsMessage: (data: JSONSerializable) => string,
   // Accepts an Error object as its only argument, and should return a string
   // representing the message to show if the API returned an error response, or
   // if some other runtime error occurred during the fetch
-  getErrorMessage: Function,
+  getErrorMessage: (error: Error) => string,
 }, dependencies: any[]): {
   // Either the return value of getNoResultsMessage(), the return value of
   // getErrorMessage(), or null, depending on the outcome of the fetch
@@ -64,7 +65,7 @@ export default function useWidgetDataFetcher({ widget, dispatch, shouldFetch, re
 
   const { isLoading, fetchError } = widget;
 
-  async function fetchWidgetData(): Promise<object> {
+  async function fetchWidgetData(): Promise<JSONSerializable> {
     dispatch({ type: 'showLoading' });
     try {
       const verseResponse = await fetch(getApiUrl(requestData)) as { json: Function };
