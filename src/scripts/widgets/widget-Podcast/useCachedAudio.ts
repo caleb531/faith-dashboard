f2ed6170f2379566aea2827b2531f3cf18f86bba
Caleb Evans <caleb@calebevans.me>
2021-12-04 18:59:38 -0800

    Add missing clarifications to useCachedAudio()

--- src/scripts/widgets/widget-Podcast/useCachedAudio.ts
+++ src/scripts/widgets/widget-Podcast/useCachedAudio.ts
@@ -1,16 +1,16 @@
 // The storage variable for the cached Audio element
 let cachedAudioElement: HTMLAudioElement = null;
 
-// Store a single HTML5 Audio element to play all audio throughout the
-// application; this is to avoid issues of multiple Audio elements playing at
-// the same time; see the PodcastAudioPlayer component for more reasons for
-// this
-function useCachedAudio(sourceUrl: string) {
+// The useCachedAudio() hook stores a single HTML5 Audio element to play all
+// audio throughout the application; this is to avoid issues of multiple Audio
+// elements playing at the same time; see the PodcastAudioPlayer component for
+// more reasons for this
+function useCachedAudio(initialSourceUrl: string): HTMLAudioElement {
 
   if (cachedAudioElement) {
     return cachedAudioElement;
   } else {
-    cachedAudioElement = new Audio(sourceUrl);
+    cachedAudioElement = new Audio(initialSourceUrl);
     return cachedAudioElement;
   }
 
