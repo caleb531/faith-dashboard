caleb531/faith-dashboard

commit 6e167989bc7dec881808b1553a109377c7910d5f

Do not repeatedly bind/unbind listeners while playing audio

src/scripts/useElementEvents.ts
@@ -15,7 +15,9 @@ function useElementEvents(element: HTMLElement, eventMap: { [key: string]: (even
         element.removeEventListener(eventType, eventMap[eventType]);
       });
     };
-  });
+    // By using an empty dependencies array, we prevent listeners from being
+    // repeatedly bound/unbound while the audio plays
+  }, []);
 
 }
 

commit e95b7327eb5547528078138fe39448f46f58d630

Fix audio seeker not updating as user dragged seeker
This bug was due to a regression in 6e16798 (and by extension, 6040f68)
that caused the timeupdate listener to latch onto the original
value of currentTime, and therefore wasn't responding to updates because
the aforementioned commits limited the listener to only being bound
once.

This new commit means reverting to a behavior where the events are bound
and unbound every second of audio, but for now, this is what needs to
happen in order for the state's latest currentTime value to be read.

src/scripts/useElementEvents.ts
@@ -15,9 +15,7 @@ function useElementEvents(element: HTMLElement, eventMap: { [key: string]: (even
         element.removeEventListener(eventType, eventMap[eventType]);
       });
     };
-    // By using an empty dependencies array, we prevent listeners from being
-    // repeatedly bound/unbound while the audio plays
-  }, [element]);
+  });
 
 }
 