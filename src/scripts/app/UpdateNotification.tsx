a2d04caf00fb7541ad8632d8e2a69eaa3d7338ba
Caleb Evans <caleb@calebevans.me>
2021-12-11 13:28:48 -0800

    Define proper dependency array for UpdateNotification useEffect
    Per <https://overreacted.io/a-complete-guide-to-useeffect/>.

--- src/scripts/app/UpdateNotification.tsx
+++ src/scripts/app/UpdateNotification.tsx
@@ -1,53 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import { messageSW, Workbox } from 'workbox-window';
+import {
+  messageSW,
+  Workbox,
+  WorkboxLifecycleWaitingEvent as WaitingEvent
+} from 'workbox-window';
+
+// Update mechanism code borrowed from
+// <https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users>
+export function initialize(wb: Workbox, setAvailableUpdate: (e: WaitingEvent) => void): void {
+  function showSkipWaitingPrompt(waitingEvent: WaitingEvent) {
+    // `waitingEvent.wasWaitingBeforeRegister` will be false if this is
+    // the first time the updated service worker is waiting.
+    // When `waitingEvent.wasWaitingBeforeRegister` is true, a previously
+    // updated service worker is still waiting.
+    // You may want to customize the UI prompt accordingly.
+    setAvailableUpdate(waitingEvent);
+  }
+  // Add an event listener to detect when the registered
+  // service worker has installed but is waiting to activate.
+  wb.addEventListener('waiting', showSkipWaitingPrompt);
+  // wb.addEventListener('externalwaiting', showSkipWaitingPrompt);
+  wb.register();
+}
+
+// Update the service worker and reload the page
+export function update(wb: Workbox, availableUpdate: WaitingEvent): void {
+  // Assuming the user accepted the update, set up a listener
+  // that will reload the page as soon as the previously waiting
+  // service worker has taken control.
+  wb.addEventListener('controlling', () => {
+    window.location.reload();
+  });
+  // Send a message to the waiting service worker instructing
+  // it to skip waiting, which will trigger the `controlling`
+  // event listener above.
+  // Note: for this to work, you have to add a message
+  // listener in your service worker. See below.
+  if (availableUpdate) {
+    messageSW(availableUpdate.sw, { type: 'SKIP_WAITING' });
+  }
+}
 
 function UpdateNotification() {
 
   const [availableUpdate, setAvailableUpdate] = useState(null);
   const [wb, setWb] = useState(() => new Workbox('service-worker.js'));
 
-  // Update mechanism code borrowed from
-  // <https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users>
-  function initialize() {
-    function showSkipWaitingPrompt(waitingEvent: object) {
-      // `waitingEvent.wasWaitingBeforeRegister` will be false if this is
-      // the first time the updated service worker is waiting.
-      // When `waitingEvent.wasWaitingBeforeRegister` is true, a previously
-      // updated service worker is still waiting.
-      // You may want to customize the UI prompt accordingly.
-      setAvailableUpdate(waitingEvent);
-    }
-    // Add an event listener to detect when the registered
-    // service worker has installed but is waiting to activate.
-    wb.addEventListener('waiting', showSkipWaitingPrompt);
-    // wb.addEventListener('externalwaiting', showSkipWaitingPrompt);
-    wb.register();
-  }
-
-  // Update the service worker and reload the page
-  function update() {
-    // Assuming the user accepted the update, set up a listener
-    // that will reload the page as soon as the previously waiting
-    // service worker has taken control.
-    wb.addEventListener('controlling', () => {
-      window.location.reload();
-    });
-    // Send a message to the waiting service worker instructing
-    // it to skip waiting, which will trigger the `controlling`
-    // event listener above.
-    // Note: for this to work, you have to add a message
-    // listener in your service worker. See below.
-    if (availableUpdate) {
-      messageSW(availableUpdate.sw, { type: 'SKIP_WAITING' });
-    }
-  }
-
   // Initialize service worker when component is first loaded
-  /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  useEffect(() => initialize(), []);
+  useEffect(() => {
+    initialize(wb, setAvailableUpdate);
+  }, [wb, setAvailableUpdate]);
 
   return (
-    <div className={`update-notification ${availableUpdate ? 'update-available' : ''}`} onClick={() => update()}>
+    <div className={`update-notification ${availableUpdate ? 'update-available' : ''}`} onClick={() => update(wb, availableUpdate)}>
       <span className="update-notification-message">Update available! Click here to update.</span>
     </div>
   );
