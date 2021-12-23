ff9cf51987554d05454c6a43d8a3f298218f12ae
Caleb Evans <caleb@calebevans.me>
2021-12-22 21:50:44 -0800

    Fix get-verse request
    It is now responding correctly.

--- src/server/widgets/widget-BibleVerse/get-verse.ts
+++ src/server/widgets/widget-BibleVerse/get-verse.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express';
-import apiInfo from './foo';
+import apiInfo from './api-credentials';
 
 export default function (app: Express): void {
+
   app.get('/widgets/bible-verse/get-verse', (req, res) => {
 
     const API_BASE_URL = 'https://api.esv.org/v3/passage/html/?';
@@ -11,6 +12,7 @@ export default function (app: Express): void {
       res.status(500);
     }
 
+    res.json({ success: true });
     res.status(200);
 
   });
