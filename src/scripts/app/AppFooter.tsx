83c656b4463a683a0b8b638c4bfb0234990588c0
Caleb Evans <caleb@calebevans.me>
2021-12-25 20:26:41 -0800

    Add Help page

--- src/scripts/app/AppFooter.tsx
+++ src/scripts/app/AppFooter.tsx
@@ -6,6 +6,8 @@ const AppFooter = React.memo(function AppFooter() {
     <footer className="app-footer">
       <small className="footer-dedication">By Caleb Evans. Dedicated to Christ our Lord</small>
       &nbsp;&middot;&nbsp;
+      <a href="help/">Help</a>
+      &nbsp;&middot;&nbsp;
       <a href="copyright/">Copyright</a>
     </footer>
   );
