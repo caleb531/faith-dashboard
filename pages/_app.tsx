a8a1fd80bc2b8d0ccb0c80c50c008acb814ca047
Caleb Evans <caleb@calebevans.me>
2022-01-19 13:00:02 -0800

    Migrate Help page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
 import '../styles/index.scss';
+import '../styles/landing-page.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
