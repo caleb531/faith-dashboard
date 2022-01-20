8b41ab547fbe56654770adfdc8f82bdf3eb59602
Caleb Evans <caleb@calebevans.me>
2022-01-19 21:05:40 -0800

    Migrate Google Tag Manager to NextJS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/index.scss';
 import '../styles/landing-page.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Initialize Google Tag Manager when page is mounted
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      TagManager.initialize({ gtmId: 'GTM-M3QKM2Z' });
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
 
