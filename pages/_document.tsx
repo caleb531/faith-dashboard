a97be02c3674b623e49b218426c4e71db951055d
Caleb Evans <caleb@calebevans.me>
2022-01-19 20:37:25 -0800

    Convert _document.tsx from a class to a functional component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,23 @@
-import Document, {
-  DocumentContext, Head, Html, Main,
-  NextScript
-} from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400&family=Merriweather:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet" />
-          <link rel="shortcut icon" href="/app-icons/favicon.png" />
-          <link rel="apple-touch-icon" href="/app-icons/apple-touch-icon.png" />
-          <link rel="manifest" href="/manifest.json" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+function AppDocument() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400&family=Merriweather:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet" />
+        <link rel="shortcut icon" href="/app-icons/favicon.png" />
+        <link rel="apple-touch-icon" href="/app-icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
-export default MyDocument;
+export default AppDocument;
