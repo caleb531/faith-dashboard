7d0b189eca5f4a46c8fba48c75b018239e6d4a6b
Caleb Evans <caleb@calebevans.me>
2022-01-20 18:21:24 -0800

    Remove obsolete sass options from NextJS config
    They seem to have no affect on how I am currently loading global styles
    into the app.

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
