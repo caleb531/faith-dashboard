57dbf135807e004f1e624add994dfeab2143162f
Caleb Evans <caleb@calebevans.me>
2022-01-22 13:29:59 -0800

    Break out AudioPlayer into sub-components

--- components/generic/audio-player/AudioPlayer.tsx
+++ components/generic/audio-player/AudioPlayer.tsx
@@ -1,11 +1,7 @@
-import { intervalToDuration } from 'date-fns';
 import React from 'react';
-import LoadingIndicator from '../../generic/LoadingIndicator';
-import useUniqueFieldId from '../../useUniqueFieldId';
+import AudioPlayerPlayPause from './AudioPlayerPlayPause';
+import AudioPlayerSeeker from './AudioPlayerSeeker';
 import useAudioLoader from './useAudioLoader';
-import useAudioPlayPause from './useAudioPlayPause';
-import useAudioSeeker from './useAudioSeeker';
-import useAudioTime from './useAudioTime';
 import useCachedAudio from './useCachedAudio';
 
 type Props = { audioElementKey: string, audioUrl: string, currentTime: number, setCurrentTime: (newCurrentTime: number) => void, isPlaying: boolean, setIsPlaying: (newIsPlaying: boolean) => void };
@@ -22,87 +18,18 @@ function AudioPlayer({ audioElementKey, audioUrl, currentTime, setCurrentTime, i
   const [audioElement] = useCachedAudio(audioElementKey);
 
   useAudioLoader(audioElement);
-  useAudioPlayPause(audioElement, isPlaying, setIsPlaying);
-  useAudioTime(audioElement, audioUrl, currentTime, setCurrentTime);
-  const { seekerProvided } = useAudioSeeker(audioElement, currentTime, setCurrentTime);
-  const seekerFieldId = useUniqueFieldId('audio-player-seeker-slider');
-
-  function toggleAudioElementPlayback() {
-    if (audioElement.paused) {
-      audioElement.play();
-    } else {
-      audioElement.pause();
-    }
-  }
-
-  // Zero-pad the given number if it's a single-digit; used for computing
-  // hh:mm:ss timestamps in the below formatSecondsAsTimestamp() function
-  function padWithZero(value: number): string {
-    return (value < 10) ? `0${value}` : String(value);
-  }
-
-  // Format the given number of seconds
-  function formatSecondsAsTimestamp(totalSeconds: number): string {
-    const { hours, minutes, seconds } = intervalToDuration({
-      start: 0,
-      end: Math.floor(totalSeconds) * 1000
-    });
-    if (hours) {
-      return [hours, padWithZero(minutes), padWithZero(seconds)].join(':');
-    } else {
-      return [minutes, padWithZero(seconds)].join(':');
-    }
-  }
 
   return (
     <div className="audio-player">
-      <button className="audio-player-playpause" onClick={() => toggleAudioElementPlayback()} disabled={!audioElement.duration}>
-        {!audioElement.duration ? (
-          <LoadingIndicator />
-        ) : (audioElement.paused) ? (
-          <img
-            className="audio-player-playpause-icon"
-            src="icons/play-light.svg"
-            alt="Play"
-            draggable="false" />
-        ) : (
-          <img
-            className="audio-player-playpause-icon"
-            src="icons/pause-light.svg"
-            alt="Pause"
-            draggable="false" />
-        )}
-      </button>
-      <div className="audio-player-seeker-container">
-        <label htmlFor={seekerFieldId} className="search accessibility-only">
-          Audio Progress
-        </label>
-        <input
-          type="range"
-          id={seekerFieldId}
-          className="audio-player-seeker-slider"
-          name="seeker"
-          min="0"
-          max={audioElement.duration || 0}
-          step="1"
-          disabled={!audioElement.duration}
-          {...seekerProvided} />
-        <div className="audio-player-time-info">
-          <span className="audio-player-current-time">
-            {!audioElement.duration ?
-              'Loading...' :
-              audioElement.currentTime >= 1 ?
-              formatSecondsAsTimestamp(Math.floor(audioElement.currentTime)) :
-              '0:00'
-            }
-          </span>
-          <span className="audio-player-time-remaining">{audioElement.duration ?
-            Math.round(audioElement.duration - audioElement.currentTime) > 0 ?
-            `-${formatSecondsAsTimestamp(audioElement.duration - audioElement.currentTime)}`
-            : '0:00'
-           : null}</span>
-        </div>
-      </div>
+      <AudioPlayerPlayPause
+        audioElement={audioElement}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying} />
+      <AudioPlayerSeeker
+        audioElement={audioElement}
+        audioUrl={audioUrl}
+        currentTime={currentTime}
+        setCurrentTime={setCurrentTime} />
     </div>
   );
 
---
+++ components/generic/audio-player/AudioPlayerSeeker.tsx
@@ -0,0 +1,69 @@
+import { intervalToDuration } from 'date-fns';
+import React from 'react';
+import useUniqueFieldId from '../../useUniqueFieldId';
+import useAudioSeeker from './useAudioSeeker';
+import useAudioTime from './useAudioTime';
+
+type Props = { audioElement: HTMLAudioElement, audioUrl: string, currentTime: number, setCurrentTime: (newCurrentTime: number) => void };
+
+function AudioPlayerSeeker({ audioElement, audioUrl, currentTime, setCurrentTime }: Props) {
+
+  const { seekerProvided } = useAudioSeeker(audioElement, currentTime, setCurrentTime);
+  useAudioTime(audioElement, audioUrl, currentTime, setCurrentTime);
+
+  // Zero-pad the given number if it's a single-digit; used for computing
+  // hh:mm:ss timestamps in the below formatSecondsAsTimestamp() function
+  function padWithZero(value: number): string {
+    return (value < 10) ? `0${value}` : String(value);
+  }
+
+  // Format the given number of seconds
+  function formatSecondsAsTimestamp(totalSeconds: number): string {
+    const { hours, minutes, seconds } = intervalToDuration({
+      start: 0,
+      end: Math.floor(totalSeconds) * 1000
+    });
+    if (hours) {
+      return [hours, padWithZero(minutes), padWithZero(seconds)].join(':');
+    } else {
+      return [minutes, padWithZero(seconds)].join(':');
+    }
+  }
+
+  const seekerFieldId = useUniqueFieldId('audio-player-seeker-slider');
+  return (
+    <div className="audio-player-seeker-container">
+      <label htmlFor={seekerFieldId} className="search accessibility-only">
+        Audio Progress
+      </label>
+      <input
+        type="range"
+        id={seekerFieldId}
+        className="audio-player-seeker-slider"
+        name="seeker"
+        min="0"
+        max={audioElement.duration || 0}
+        step="1"
+        disabled={!audioElement.duration}
+        {...seekerProvided} />
+      <div className="audio-player-time-info">
+        <span className="audio-player-current-time">
+          {!audioElement.duration ?
+            'Loading...' :
+            audioElement.currentTime >= 1 ?
+            formatSecondsAsTimestamp(Math.floor(audioElement.currentTime)) :
+            '0:00'
+          }
+        </span>
+        <span className="audio-player-time-remaining">{audioElement.duration ?
+          Math.round(audioElement.duration - audioElement.currentTime) > 0 ?
+          `-${formatSecondsAsTimestamp(audioElement.duration - audioElement.currentTime)}`
+          : '0:00'
+          : null}</span>
+      </div>
+    </div>
+  );
+
+}
+
+export default AudioPlayerSeeker;
