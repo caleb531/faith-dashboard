68af6e5ad221b189a51da3eeb79cb627840ef4e3
Caleb Evans <caleb@calebevans.me>
2022-01-23 17:28:48 -0800

    Fix middleware to retrieve "true" host of request
    Per my comment in the code, `req.headers.get('host')` returns the host
    from the client's perspective (e.g. 'faithdashboard.com'), whereas
    `req.nextUrl.host` is always 'localhost:3000' (the host from the Next
    server's perspective).

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,16 +2,18 @@ import { NextRequest, NextResponse } from 'next/server';
 
 // Force-redirect every HTTP request to HTTPS
 function forceHTTPS(req: NextRequest) {
-  console.log('protocol', req.nextUrl.protocol);
-  console.log('hostname', req.nextUrl.hostname);
-  console.log('headers', req.headers);
   if (process.env.NODE_ENV === 'production' &&
     req.headers.get('x-forwarded-proto') !== 'https'
     &&
-    req.nextUrl.hostname !== 'localhost'
+    // This check prevents us from getting trapped in HTTPS localhost if we are
+    // testing a production build locally via `next build && next start`; we
+    // can use `req.headers.get('host')` to get the true host (e.g.
+    // 'faithdashboard.com'), whereas `req.nextUrl.host` is always
+    // 'localhost:3000'
+    !req.headers.get('host').includes('localhost')
   ) {
     return NextResponse.redirect(
-      `https://${req.nextUrl.host}${req.nextUrl.pathname}`,
+      `https://${req.headers.get('host')}${req.nextUrl.pathname}`,
       301
     );
   }
@@ -19,7 +21,7 @@ function forceHTTPS(req: NextRequest) {
 
 // Redirect every www request to the non-www equivalent
 function redirectWwwToNonWww(req: NextRequest) {
-  const host = String(req.nextUrl.host);
+  const host = req.headers.get('host');
   const wwwRegex = /^www\./;
   if (wwwRegex.test(host)) {
     const newHost = host.replace(wwwRegex, '');
