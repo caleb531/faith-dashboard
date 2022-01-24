8e4d3b90c4a4a268444a07f67eedfa5e22fe1f18
Caleb Evans <caleb@calebevans.me>
2022-01-23 16:29:58 -0800

    Force-redirect plain HTTP to HTTPS

---
+++ pages/_middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// Force-redirect plain HTTP to HTTPS
+export function middleware(req: NextRequest) {
+    if (
+      process.env.NODE_ENV === 'production'
+      &&
+      req.headers.get('x-forwarded-proto') !== 'https'
+      &&
+      req.nextUrl.hostname !== 'localhost'
+    ) {
+        return NextResponse.redirect(
+           `https://${req.nextUrl.host}${req.nextUrl.pathname}`,
+           301
+        );
+    }
+    return NextResponse.next();
+}
