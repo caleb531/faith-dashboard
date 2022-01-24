7e056444bcfd1a9c6922d75e809ec9d044c3130e
Caleb Evans <caleb@calebevans.me>
2022-01-23 16:34:24 -0800

    Bring develop up-to-date with master

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,17 +2,17 @@ import { NextRequest, NextResponse } from 'next/server';
 
 // Force-redirect plain HTTP to HTTPS
 export function middleware(req: NextRequest) {
-    if (
-      process.env.NODE_ENV === 'production'
-      &&
-      req.headers.get('x-forwarded-proto') !== 'https'
-      &&
-      req.nextUrl.hostname !== 'localhost'
-    ) {
-        return NextResponse.redirect(
-           `https://${req.nextUrl.host}${req.nextUrl.pathname}`,
-           301
-        );
-    }
-    return NextResponse.next();
+  if (
+    process.env.NODE_ENV === 'production'
+    &&
+    req.headers.get('x-forwarded-proto') !== 'https'
+    &&
+    req.nextUrl.hostname !== 'localhost'
+  ) {
+    return NextResponse.redirect(
+      `https://${req.nextUrl.host}${req.nextUrl.pathname}`,
+      301
+    );
+  }
+  return NextResponse.next();
 }
