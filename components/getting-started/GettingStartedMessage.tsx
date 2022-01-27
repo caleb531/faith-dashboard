cf30e2131c187abe3ae9ccfdf95c0d15e659f004
Caleb Evans <caleb@calebevans.me>
2022-01-26 21:16:50 -0800

    Scroll highlighted element into view

--- components/getting-started/GettingStartedMessage.tsx
+++ components/getting-started/GettingStartedMessage.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import GettingStartedContext from './GettingStartedContext';
 
 type Props = { isCurrentStep: boolean };
@@ -7,13 +7,31 @@ type Props = { isCurrentStep: boolean };
 function GettingStartedMessage({ isCurrentStep }: Props) {
 
   const { currentStep, currentStepIndex, moveToNextStep, skipGettingStarted } = useContext(GettingStartedContext);
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  // Scroll the highlighted element into view when that respective Getting
+  // Started step is active
+  useEffect(() => {
+    if (isCurrentStep) {
+      messageRef.current.scrollIntoView({
+        // Animate the scrolling
+        behavior: 'smooth',
+        // Try to center the viewport around the message tooltip so that the
+        // highlighted element is increasingly likely to be in view
+        block: 'center'
+      });
+    }
+  }, [isCurrentStep]);
 
   return (
-    isCurrentStep ? <div className={classNames(
-      'getting-started-message',
-      `position-${currentStep.position}`,
-      `alignment-${currentStep.alignment}`
-    )} style={{ width: currentStep.width || 'auto' }}>
+    isCurrentStep ? <div
+        className={classNames(
+        'getting-started-message',
+        `position-${currentStep.position}`,
+        `alignment-${currentStep.alignment}`
+      )}
+      style={{ width: currentStep.width || 'auto' }}
+      ref={messageRef}>
       <span className="getting-started-message-text">{currentStep.message}</span>
       <div className="getting-started-message-controls">
         <button
