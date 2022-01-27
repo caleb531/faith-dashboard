caleb531/faith-dashboard

commit 49c456a34da34d660f9c79f2d16d0341d3c0c9e1

Fix <head> not being rendered in SSR context
The <AppHead /> component should not be encapsulated in the isMounted
check.

components/app/App.tsx
@@ -43,9 +43,9 @@ function App() {
   const isMounted = useMountListener();
   return (
     <AppContext.Provider value={dispatchToApp}>
-      {isMounted ? <GettingStartedFlow shouldShow={app.shouldShowGettingStarted}>
         <AppHead />
         <div className={`app theme-${app.theme} ${isTouchDevice() ? 'is-touch-device' : 'is-not-touch-device'}`}>
+          {isMounted ? <GettingStartedFlow shouldShow={app.shouldShowGettingStarted}>
             {shouldLoadServiceWorker() ? (
               <UpdateNotification />
             ) : null}
@@ -54,8 +54,8 @@ function App() {
               <WidgetBoard widgets={app.widgets} />
             </Suspense>
             <AppFooter />
+          </GettingStartedFlow> : null}
         </div>
-      </GettingStartedFlow> : null}
     </AppContext.Provider>
   );
 

commit 3b03b8aa7f27f96883e032e9256cbd5b468d4db7

Fix initial render mismatch between client and SSR
This was a regression caused by 49c456a.

components/app/App.tsx
@@ -44,8 +44,8 @@ function App() {
   return (
     <AppContext.Provider value={dispatchToApp}>
         <AppHead />
-        <div className={`app theme-${app.theme} ${isTouchDevice() ? 'is-touch-device' : 'is-not-touch-device'}`}>
-          {isMounted ? <GettingStartedFlow shouldShow={app.shouldShowGettingStarted}>
+        {isMounted ? <div className={`app theme-${app.theme} ${isTouchDevice() ? 'is-touch-device' : 'is-not-touch-device'}`}>
+          <GettingStartedFlow shouldShow={app.shouldShowGettingStarted}>
             {shouldLoadServiceWorker() ? (
               <UpdateNotification />
             ) : null}
@@ -54,8 +54,8 @@ function App() {
               <WidgetBoard widgets={app.widgets} />
             </Suspense>
             <AppFooter />
-          </GettingStartedFlow> : null}
-        </div>
+          </GettingStartedFlow>
+        </div> : null}
     </AppContext.Provider>
   );
 