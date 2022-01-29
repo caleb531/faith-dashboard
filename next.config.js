a1146abeb84ff4714e491d344a4b0727d6cea184
Caleb Evans <caleb@calebevans.me>
2022-01-28 20:40:58 -0800

    Fix bad-precaching-response on _middleware.js

--- next.config.js
+++ next.config.js
@@ -29,7 +29,8 @@ const nextConfig = withPWA({
     runtimeCaching,
     buildExcludes: [
       /middleware-manifest\.json$/,
-      /middleware-runtime\.js$/
+      /middleware-runtime\.js$/,
+      /_middleware\.js$/
     ]
   },
   async headers() {
