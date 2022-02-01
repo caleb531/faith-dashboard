3f8808baf73083a7695bee2d693752f5e90c44d4
Caleb Evans <caleb@calebevans.me>
2022-01-31 18:23:36 -0800

    Rework app footer

--- components/app/AppFooter.tsx
+++ components/app/AppFooter.tsx
@@ -9,16 +9,21 @@ function AppFooter() {
   return (
     <footer className="app-footer">
       <small className="app-footer-dedication">By <a href="https://calebevans.me/">Caleb Evans</a>. Dedicated to Christ our Lord.</small>
-      <div className="app-footer-links">
-        <span className="app-footer-help-container">
-        {isCurrentStep ? <TutorialMessage /> : null}
-        <a href="help/" {...stepProps}>Help</a>
-        </span>
-        &nbsp;&middot;&nbsp;
-        <a href="https://github.com/caleb531/faith-dashboard">GitHub</a>
-        &nbsp;&middot;&nbsp;
-        <a href="copyright/">Copyright</a>
-      </div>
+      <ul className="app-footer-links">
+        <li className="app-footer-link-help">
+          {/* The inner container is so that the tutorial bubble message can be properly positioned (because the :after on the parent skews its own width) */}
+          <span className="app-footer-link-help-inner" {...stepProps}>
+            {isCurrentStep ? <TutorialMessage /> : null}
+            <a href="help/">Help</a>
+          </span>
+        </li>
+        <li className="app-footer-link">
+          <a href="https://github.com/caleb531/faith-dashboard">GitHub</a>
+        </li>
+        <li className="app-footer-link">
+          <a href="copyright/">Copyright</a>
+        </li>
+      </ul>
     </footer>
   );
 
