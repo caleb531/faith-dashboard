d7d2a9f810e270b6560a176a21fd32afd715b62e
Caleb Evans <caleb@calebevans.me>
2022-01-31 17:55:20 -0800

    Prevent zoom-in when focusing inputs on iOS
    The user should still be able to zoom in manually via the pinch-to-zoom
    gesture; this is just a matter of preventing the app from automatically
    zooming in.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import React, { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
 import '../styles/index.scss';
@@ -11,7 +12,15 @@ function AppWrapper({ Component, pageProps }: AppProps) {
       TagManager.initialize({ gtmId: 'GTM-M3QKM2Z' });
     }
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Prevent viewport from zooming in when focusing inputs on iOS; according to StackOverflow, this meta tag must be added to _app instead of _document in order for proper de-duping (since NextJS already adds a 'meta viewport' tag to the page by default); source: https://stackoverflow.com/a/65833542/560642 */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default AppWrapper;
