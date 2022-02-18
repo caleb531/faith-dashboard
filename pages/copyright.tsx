de2176d3bdb919d5e8c45602a8424f3296683078
Caleb Evans <caleb@calebevans.me>
2022-02-18 13:12:17 -0800

    Add copyright info for Unsplash
    Not required by their service, but will at least do the courtesy of
    mentioning them, since I already have a Copyright page.

--- pages/copyright.tsx
+++ pages/copyright.tsx
@@ -22,6 +22,8 @@ function Copyright() {
 
       <p>Scripture quotations marked “ESV” are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. You may not copy or download more than 500 consecutive verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
 
+      <p>Photo backgrounds provided by <a href="https://unsplash.com/">Unsplash</a>.</p>
+
       <p><Link href="/">Return to App</Link></p>
     </article>
   );
