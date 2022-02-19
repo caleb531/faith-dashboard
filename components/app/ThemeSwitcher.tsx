a64c1ea15e2b9a18001ba08a6f6839f049d98729
Caleb Evans <caleb@calebevans.me>
2022-02-18 21:08:06 -0800

    Display available themes in modal

--- components/app/ThemeSwitcher.tsx
+++ components/app/ThemeSwitcher.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import Image from 'next/image';
 import React from 'react';
 import Modal from '../generic/Modal';
 import { AppTheme } from './app.d';
@@ -14,15 +15,27 @@ function ThemeSwitcher({ currentTheme, onCloseThemeSwitcher }: Props) {
         <section className="theme-switcher">
           <h1>Choose Theme</h1>
           <p>Click a theme to use it for your dashboard!</p>
+          <h2>You can pick a photo...</h2>
           <ul className="theme-switcher-list theme-switcher-photo-themes">
             {photoThemeList.map((themeListItem) => {
               return (
-                <li key={themeListItem.value} className="theme-switcher-list-item theme-switcher-photo-theme">
-
+                <li
+                  key={themeListItem.value}
+                  className="theme-switcher-list-item theme-switcher-photo-theme">
+                  <div className="theme-switcher-photo-theme-photo-container">
+                    <Image
+                      className="theme-switcher-photo-theme-photo"
+                      src={`/images/background-photos/${themeListItem.value}.jpg`}
+                      alt=""
+                      width="200"
+                      height="100" />
+                  </div>
+                  <span className="theme-switcher-label">{themeListItem.label}</span>
                 </li>
               );
             })}
           </ul>
+          <h2>...or pick a color...</h2>
           <ul className="theme-switcher-list theme-switcher-color-themes">
             {colorThemeList.map((themeListItem) => {
               return (
@@ -31,7 +44,7 @@ function ThemeSwitcher({ currentTheme, onCloseThemeSwitcher }: Props) {
                     'theme-switcher-color-theme-swatch',
                     `theme-${themeListItem.value}`
                   )}></div>
-                  <span className="theme-switcher-color-theme-label">{themeListItem.label}</span>
+                  <span className="theme-switcher-label">{themeListItem.label}</span>
                 </li>
               );
             })}
