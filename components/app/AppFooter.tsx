30edfb8662a0fd07e55615aaa26ce11abb7313c3
Caleb Evans <caleb@calebevans.me>
2022-02-20 19:41:16 -0800

    Fix alignment of "Help" step message in tutorial

--- components/app/AppFooter.tsx
+++ components/app/AppFooter.tsx
@@ -11,15 +11,15 @@ function AppFooter() {
     <footer className="app-footer">
       <small className="app-footer-dedication">By <a href="https://calebevans.me/">Caleb Evans</a>. Dedicated to Christ our Lord.</small>
       <ul className="app-footer-links">
+        <li className="app-footer-link">
+          <a href="https://twitter.com/faithdashboard">@faithdashboard</a>
+        </li>
         <li className="app-footer-link-help">
           {/* The inner container is so that the tutorial bubble message can be properly positioned (because the :after on the parent skews its own width) */}
           <span className="app-footer-link-help-inner" {...stepProps}>
             {isCurrentStep ? <TutorialStepMessage /> : null}
             <Link href="help/">Help</Link>
           </span>
-        </li>
-        <li className="app-footer-link">
-          <a href="https://twitter.com/faithdashboard">@faithdashboard</a>
         </li>
         <li className="app-footer-link">
           <Link href="copyright/">Copyright</Link>
