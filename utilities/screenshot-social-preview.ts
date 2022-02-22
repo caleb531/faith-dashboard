dcbb67c30ae4ecb2185bb49944dc8860f32750c7
Caleb Evans <caleb@calebevans.me>
2022-02-22 12:58:53 -0800

    Write utility to generate social preview image

---
+++ utilities/screenshot-social-preview.ts
@@ -0,0 +1,48 @@
+import puppeteer from 'puppeteer';
+
+// The non-retina dimensions of the social preview image to generate
+const screenshotWidth = 1200;
+const screenshotHeight = 630;
+// The path to the social preview image to generate
+const screenshotPath = './public/images/social-preview.png';
+
+async function main(): Promise<void> {
+
+  console.log('launching browser...');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  console.log('setting viewport...');
+  await page.setViewport({
+    width: screenshotWidth,
+    height: screenshotHeight,
+    deviceScaleFactor: 2
+  });
+
+  console.log('navigating to app...');
+  await page.goto('http://localhost:3000');
+
+  console.log('configuring dashboard...');
+  await page.evaluate(async (_) => {
+    const socialPreviewAppState = await (await fetch('./social-preview-app-state.json')).json();
+    localStorage.setItem(
+      'faith-dashboard-app',
+      JSON.stringify(socialPreviewAppState)
+    );
+  });
+
+  console.log('waiting for dashboard to fully load...');
+  await page.reload({ waitUntil: ['networkidle0', 'load'] });
+
+  console.log('generating screenshot...');
+  await page.screenshot({
+    path: screenshotPath,
+    type: 'png',
+    captureBeyondViewport: false
+  });
+
+  console.log('closing browser...');
+  await browser.close();
+
+}
+main();
