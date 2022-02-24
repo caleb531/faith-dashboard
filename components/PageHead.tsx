44fb8e4351273bf0a81cb84d4ea47a2a3f36c3f5
Caleb Evans <caleb@calebevans.me>
2022-02-23 16:31:55 -0800

    Move general site data to separate file

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -1,37 +1,35 @@
 import Head from 'next/head';
 import React from 'react';
+import site from './siteData';
 
 type Props = { pagePath: string, pageTitle: string, pageDescription: string };
 
 // The <head> content that should appear on all pages; the props for the
 // current page are passed into this function to maximize SEO for each page
 function PageHead({ pagePath, pageTitle, pageDescription }: Props) {
-  const baseUrl = 'https://faithdashboard.com';
-  const url = `${baseUrl}${pagePath}`;
-  const image = `${baseUrl}/images/social-preview.jpg?v=2`;
-  const twitterUsernameSite = '@faithdashboard';
-  const twitterUsernameCreator = '@caleb531';
+  const pageUrl = `${site.baseUrl}${pagePath}`;
+  const socialImageUrl = `${site.baseUrl}${site.socialImagePath}`;
   return (
     <Head>
         {/* Prevent viewport from zooming in when focusing inputs on iOS; according to StackOverflow, this meta tag must be added to _app instead of _document in order for proper de-duping (since NextJS already adds a 'meta viewport' tag to the page by default); source: https://stackoverflow.com/a/65833542/560642 */}
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <title>{pageTitle}</title>
-        {pagePath ? <link rel="canonical" href={url} /> : null}
-        <meta property="og:title" content={pageTitle} />
+        <title>{pageTitle || site.title}</title>
+        {pagePath ? <link rel="canonical" href={pageUrl} /> : null}
+        <meta property="og:title" content={pageTitle || site.title} />
         <meta property="og:type" content="website" />
-        {pagePath ? <meta property="og:url" content={url} /> : null}
-        <meta property="og:image" content={image} />
+        {pagePath ? <meta property="og:url" content={pageUrl} /> : null}
+        <meta property="og:image" content={socialImageUrl} />
         <meta property="og:image:width" content="2400" />
         <meta property="og:image:height" content="1260" />
         {pageDescription ? <meta property="og:description" content={pageDescription} /> : null}
         {pageDescription ? <meta name="description" content={pageDescription} /> : null}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={twitterUsernameSite} />
-        <meta name="twitter:creator" content={twitterUsernameCreator} />
-        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:site" content={site.twitterUsernameSite} />
+        <meta name="twitter:creator" content={site.twitterUsernameCreator} />
+        <meta name="twitter:title" content={pageTitle || site.title} />
         {pageDescription ? <meta name="twitter:description" content={pageDescription} /> : null}
-        <meta name="twitter:image" content={image} />
-        {pagePath ? <meta name="twitter:url" content={url} /> : null}
+        <meta name="twitter:image" content={socialImageUrl} />
+        {pagePath ? <meta name="twitter:url" content={pageUrl} /> : null}
     </Head>
   );
 }
---
+++ components/siteData.ts
@@ -0,0 +1,8 @@
+// Generic site data
+export default {
+  baseUrl: 'https://faithdashboard.com',
+  title: 'Faith Dashboard',
+  socialImagePath: '/images/social-preview.jpg?v=2',
+  twitterUsernameSite: '@faithdashboard',
+  twitterUsernameCreator: '@caleb531'
+};
