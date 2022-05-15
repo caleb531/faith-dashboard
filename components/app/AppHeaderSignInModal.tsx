84c14b540bbd9dab80164055414d3892f81b8f9e
Caleb Evans <caleb@calebevans.me>
2022-05-15 16:53:19 -0700

    Build basic registration / sign-in form

--- components/app/AppHeaderSignInModal.tsx
+++ components/app/AppHeaderSignInModal.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from '../generic/Modal';
+import { supabase } from '../supabaseClient';
 
 type Props = {
   onCloseSignInModal: () => void
@@ -9,8 +10,16 @@ function AppHeaderSignInModal({
   onCloseSignInModal
 }: Props) {
   const [isFormShowing, setIsFormShowing] = useState(false);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   function showSignInForm() {
     setIsFormShowing(true);
+  }
+  function submitForm(event: React.FormEvent) {
+    event.preventDefault();
+    const formElement = event.currentTarget as HTMLFormElement;
+    const emailInput = formElement.elements.namedItem('email') as HTMLInputElement;
+    supabase.auth.signIn({ email: emailInput.value });
+    setIsFormSubmitted(true);
   }
   return (
     <Modal onCloseModal={onCloseSignInModal}>
@@ -18,8 +27,15 @@ function AppHeaderSignInModal({
         <h1>Account</h1>
         <p>Create a Faith Dashboard account to sync your dashboard and gain other features!</p>
         {isFormShowing ? (
-          <form className="sign-in-form">
-            <input type="text" name="email" />
+          <form className="sign-in-form" onSubmit={submitForm}>
+            <input
+              className="sign-in-form-email"
+              type="email"
+              name="email"
+              placeholder="Your email address"
+              autoFocus
+              />
+              <button type="submit" className="sign-in-submit">Submit</button>
           </form>
         ) : (
           <div className="sign-in-cta-container">
