4ca9a37e63195f7551d9e60c2e7e649a0d6260d2
Caleb Evans <caleb@calebevans.me>
2022-05-22 18:45:50 -0700

    Rewrite theme code using CSS variables / custom properties

--- components/app/useThemeForEntirePage.ts
+++ components/app/useThemeForEntirePage.ts
@@ -1,16 +1,32 @@
+import { groupBy } from 'lodash-es';
 import useIsomorphicLayoutEffect from '../useIsomorphicLayoutEffect';
 import { AppTheme } from './app.d';
+import colorThemeList from './appColorThemeList';
+import photoThemeList from './appPhotoThemeList';
+
+// Generate lookup tables of the different theme types so we can determine
+// which specific theme belongs to which theme type
+const colorThemesById = groupBy(colorThemeList, 'value');
+const photoThemesById = groupBy(photoThemeList, 'value');
 
 // The useThemeForEntirePage() hook takes the given color theme and applies it
 // to the <body> element of the page
 export default function useThemeForEntirePage(theme: AppTheme) {
 
   useIsomorphicLayoutEffect(() => {
-    document.body.classList.add(`theme-${theme}`);
+    document.body.classList.add(
+      'theme',
+      `theme-${theme}`,
+      colorThemesById[theme] ?
+        'color-theme' :
+        photoThemesById[theme] ?
+          'photo-theme' :
+          ''
+    );
     return () => {
-      // Remove any previous theme-* classes applied to the <body>
+      // Remove any previous theme-* or *-theme classes applied to the <body>
       Array.from(document.body.classList).forEach((className) => {
-        if (/^theme-/.test(className)) {
+        if (/^((theme-\w+)|(\w+-theme))/.test(className)) {
           document.body.classList.remove(className);
         }
       });
