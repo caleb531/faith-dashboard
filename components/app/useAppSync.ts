e317adb3ae79c1d4e6fffd122db9663e8a4780b0
Caleb Evans <caleb@calebevans.me>
2022-05-27 18:14:27 -0700

    Remove obsolete helper functions for useAppSync() and useWidgetSync()
    These have been replaced by the useSyncPush() hook.

--- components/app/useAppSync.ts
+++ components/app/useAppSync.ts
@@ -3,23 +3,6 @@ import { supabase } from '../supabaseClient';
 import useSyncPush from '../useSyncPush';
 import { AppState } from './app.d';
 
-// Send the supplied app data to the apps table in the database
-async function pushAppToDatabase(app: AppState): Promise<void> {
-  const user = supabase.auth.user();
-  if (!user) {
-    return;
-  }
-  await supabase
-    .from('dashboards')
-    .upsert([
-      {
-        id: app.id,
-        user_id: user.id,
-        raw_data: JSON.stringify(app)
-      }
-    ]);
-}
-
 // The useAppSync() hook
 function useAppSync(
   app: AppState
--- components/widgets/useWidgetSync.ts
+++ components/widgets/useWidgetSync.ts
@@ -2,23 +2,6 @@ import { supabase } from '../supabaseClient';
 import useSyncPush from '../useSyncPush';
 import { WidgetState } from './widget';
 
-// Send the supplied widget data to the widgets table in the database
-async function pushWidgetToDatabase(widget: WidgetState): Promise<void> {
-  const user = supabase.auth.user();
-  if (!user) {
-    return;
-  }
-  await supabase
-    .from('widgets')
-    .upsert([
-      {
-        id: widget.id,
-        user_id: user.id,
-        raw_data: JSON.stringify(widget)
-      }
-    ]);
-}
-
 // The useWidgetSync() hook pushes the state of the given widget to the server
 // whenever it changes
 function useWidgetSync(
