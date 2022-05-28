25da38fec31b5ec48a32b9b1a7087435325d47cf
Caleb Evans <caleb@calebevans.me>
2022-05-28 13:05:13 -0700

    Replace tiny-typed-emitter with OG tiny-emitter
    The original tiny-emitter package does not rely on any Node APIs (unlike
    tiny-typed-emitter, which relies of the Node 'event' module).

--- components/widgets/widgetSyncService.ts
+++ components/widgets/widgetSyncService.ts
@@ -1,16 +1,12 @@
-import { TypedEmitter } from 'tiny-typed-emitter';
+import { TinyEmitter } from 'tiny-emitter';
 import { WidgetState } from './widget';
 
 // The callback for a widget sync event
 type WidgetSyncCallback = (state: WidgetState) => void;
-
-interface WidgetSyncEvents {
-  // The key names map to dynamic event names based on the widget ID (e.g.
-  // 'pull:4a168299-dd55-4dfb-8a2f-58327073d7d7')
-  [key: string]: WidgetSyncCallback
-}
-
-const widgetSyncEmitter = new TypedEmitter<WidgetSyncEvents>();
+// A global emitter object used for all widgets; the ID of each event listened
+// for will be dynamic based on the widget ID (e.g.
+// 'pull:4a168299-dd55-4dfb-8a2f-58327073d7d7')
+const widgetSyncEmitter = new TinyEmitter();
 
 // Broadcast the widget state from the server to the local component for that
 // widget
@@ -21,16 +17,12 @@ export function broadcastToWidget(widgetId: string, widget: WidgetState) {
 // Listen for broadcasts that this particular widget had its state recently
 // pulled from the server
 export function onPull(widgetId: string, callback: WidgetSyncCallback) {
-  return widgetSyncEmitter.addListener(`pull:${widgetId}`, callback);
+  return widgetSyncEmitter.on(`pull:${widgetId}`, callback);
 }
 
 // Remove all listeners bound with onPull() for this widget
 export function offPull(widgetId: string, callback?: WidgetSyncCallback) {
-  if (callback) {
-    return widgetSyncEmitter.removeListener(`pull:${widgetId}`, callback);
-  } else {
-    return widgetSyncEmitter.removeAllListeners(`pull:${widgetId}`);
-  }
+  return widgetSyncEmitter.off(`pull:${widgetId}`, callback);
 }
 
 export default { onPull, offPull };
