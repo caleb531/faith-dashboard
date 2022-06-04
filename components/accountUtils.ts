ec343a595e1c134cc4f99e72fb3332b1f81ff37b
Caleb Evans <caleb@calebevans.me>
2022-06-03 19:02:36 -0700

    Improve code style for isSessionActive() function

--- components/accountUtils.ts
+++ components/accountUtils.ts
@@ -7,13 +7,13 @@ const MS_IN_S = 1000;
 
 // Return true if the user is signed in with a session that isn't yet expired;
 // return false otherwise
-export function isSessionActive(session: Session | null = supabase.auth.session()) {
+export function isSessionActive(
+  session: Session | null = supabase.auth.session()
+) {
   return (
     session
     &&
     session.expires_at
-    &&
-    session.expires_in
     &&
     session.expires_at > (Date.now() / MS_IN_S)
   );
