714f86d0555ba06ed08e51140a3072735cf364cd
Caleb Evans <caleb@calebevans.me>
2022-06-16 20:29:27 -0700

    Error when Captcha site key is missing
    This error is only thrown on pages where the Captcha is actually used,
    but could be a handy debugging behavior, nonetheless.

--- components/Captcha.tsx
+++ components/Captcha.tsx
@@ -1,5 +1,9 @@
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 
+if (!process.env.NEXT_PUBLIC_GOTRUE_SECURITY_CAPTCHA_SITEKEY) {
+  throw new Error('NEXT_PUBLIC_GOTRUE_SECURITY_CAPTCHA_SITEKEY environment variable missing');
+}
+
 type Props = {
   setCaptchaToken: (token: string) => void
 }
