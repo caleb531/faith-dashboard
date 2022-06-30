2e114ca7cb1017398e7b29c081b57b7e484cb22f
Caleb Evans <caleb@calebevans.me>
2022-06-29 20:44:13 -0700

    Write more tutorial tests

--- components/__tests__/Tutorial.test.tsx
+++ components/__tests__/Tutorial.test.tsx
@@ -3,25 +3,37 @@ import { fireEvent, render } from '@testing-library/react';
 import Home from '../../pages/index';
 
 describe('Tutorial', function () {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
   it('should render', function () {
     const { container } = render(<Home />);
     expect(container.querySelector('.tutorial-step-tooltip')).toContainHTML(
       'Welcome'
     );
   });
+
   it('should skip', function () {
-    const { container } = render(<Home />);
+    const { container, getByText } = render(<Home />);
     expect(
       container.querySelector('.tutorial-step-tooltip')
     ).toBeInTheDocument();
-    const skipButton = container.querySelector(
-      '.tutorial-step-tooltip-control.warning'
-    );
-    if (skipButton) {
-      fireEvent.click(skipButton);
-    }
+    fireEvent.click(getByText('Skip Tutorial'));
     expect(
       container.querySelector('.tutorial-step-tooltip')
     ).not.toBeInTheDocument();
+  });
+
+  it('should advance tutorial', function () {
+    const { container, getByText } = render(<Home />);
+    expect(
+      container.querySelector('.tutorial-step-tooltip')
+    ).toBeInTheDocument();
+    fireEvent.click(getByText('Get Started'));
+    expect(getByText('Next')).toBeInTheDocument();
+    expect(container.querySelector('.tutorial-step-tooltip')).toContainHTML(
+      'This is your dashboard'
+    );
   });
 });
