c1da434af72e85ed3335cc1093b31f6c368230b8
Caleb Evans <caleb@calebevans.me>
2022-07-01 19:54:54 -0700

    Add descriptive text to Loading icon
    For accessibility.

--- components/reusable/LoadingIndicator.tsx
+++ components/reusable/LoadingIndicator.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-
 function LoadingIndicator() {
   return (
     <div className="loading-indicator">
       <svg viewBox="0 0 32 32" className="loading-indicator-icon">
+        <title>Loading...</title>
         <path d="M 16,2 A 8,8 0,0,1 16,30" />
       </svg>
     </div>
