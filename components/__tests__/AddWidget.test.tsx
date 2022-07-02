fd0470115bc3c5ba9a36ef748a237c21cfa01dae
Caleb Evans <caleb@calebevans.me>
2022-07-02 14:50:10 -0700

    Streamline Add Widget UI tests

--- components/__tests__/AddWidget.test.tsx
+++ components/__tests__/AddWidget.test.tsx
@@ -2,53 +2,47 @@ import '@testing-library/jest-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Home from '../../pages/index';
+import { WidgetTypeId } from '../widgets/widget';
+import widgetTypes from '../widgets/widgetTypes';
+
+async function addWidget(widgetTypeId: WidgetTypeId) {
+  const widgetType = widgetTypes.find((widgetType) => {
+    return widgetType.type === widgetTypeId;
+  });
+  if (!widgetType) {
+    throw new Error(`Invalid widget type: ${widgetTypeId}`);
+  }
+  render(<Home />);
+  await waitFor(async () => {
+    expect(screen.getAllByRole('article')).toHaveLength(4);
+  });
+  await userEvent.click(screen.getByRole('button', { name: 'Add Widget' }));
+  await userEvent.click(
+    screen.getByRole('button', { name: `Add ${widgetType.name} Widget` })
+  );
+}
+
+async function validateAddedWidget(widgetTypeId: WidgetTypeId) {
+  expect(screen.getAllByRole('article')).toHaveLength(5);
+  expect(screen.getAllByRole('article')[0]).toHaveProperty(
+    'dataset.widgetType',
+    widgetTypeId
+  );
+}
 
 describe('Add Widget UI', () => {
   it('should add BibleVerse widget', async () => {
-    render(<Home />);
-    await waitFor(async () => {
-      expect(screen.getAllByRole('article')).toHaveLength(4);
-    });
-    await userEvent.click(screen.getByRole('button', { name: 'Add Widget' }));
-    await userEvent.click(
-      screen.getByRole('button', { name: 'Add Bible Verse Widget' })
-    );
-    expect(screen.getAllByRole('article')).toHaveLength(5);
-    expect(screen.getAllByRole('article')[0]).toHaveProperty(
-      'dataset.widgetType',
-      'BibleVerse'
-    );
+    await addWidget('BibleVerse');
+    await validateAddedWidget('BibleVerse');
   });
 
   it('should add Note widget', async () => {
-    render(<Home />);
-    await waitFor(async () => {
-      expect(screen.getAllByRole('article')).toHaveLength(4);
-    });
-    await userEvent.click(screen.getByRole('button', { name: 'Add Widget' }));
-    await userEvent.click(
-      screen.getByRole('button', { name: 'Add Note Widget' })
-    );
-    expect(screen.getAllByRole('article')).toHaveLength(5);
-    expect(screen.getAllByRole('article')[0]).toHaveProperty(
-      'dataset.widgetType',
-      'Note'
-    );
+    await addWidget('Note');
+    await validateAddedWidget('Note');
   });
 
   it('should add Podcast widget', async () => {
-    render(<Home />);
-    await waitFor(async () => {
-      expect(screen.getAllByRole('article')).toHaveLength(4);
-    });
-    await userEvent.click(screen.getByRole('button', { name: 'Add Widget' }));
-    await userEvent.click(
-      screen.getByRole('button', { name: 'Add Podcast Widget' })
-    );
-    expect(screen.getAllByRole('article')).toHaveLength(5);
-    expect(screen.getAllByRole('article')[0]).toHaveProperty(
-      'dataset.widgetType',
-      'Podcast'
-    );
+    await addWidget('Podcast');
+    await validateAddedWidget('Podcast');
   });
 });
