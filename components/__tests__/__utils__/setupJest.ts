a951fbb2318b4e5dd622f9629f05b8d59cc24b39
Caleb Evans <caleb@calebevans.me>
2022-07-02 17:05:58 -0700

    Increase timeout of Jest tests
    An attempt to resolve the timing issues when running tests on GitHub CI,
    even though the tests are passing locally.

--- components/__tests__/__utils__/setupJest.ts
+++ components/__tests__/__utils__/setupJest.ts
@@ -1,5 +1,12 @@
+import { configure } from '@testing-library/dom';
 import fetch, { enableFetchMocks } from 'jest-fetch-mock';
 import podcastSearchJson from '../__json__/podcastSearch.json';
+
+// Increase timeout of React Testing Library's waitFor() function, as well as
+// Jest's global max timeout; this is an attempt to resolve the 'Unable to find
+// role' error when running tests on CI (even though all tests pass locally)
+configure({ asyncUtilTimeout: 5000 });
+jest.setTimeout(10000);
 
 enableFetchMocks();
 
