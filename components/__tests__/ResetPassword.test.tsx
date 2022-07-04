26a8756e5ee04c5f0a8621b377acbe3f60031da7
Caleb Evans <caleb@calebevans.me>
2022-07-04 13:13:45 -0700

    Add tests for Reset Password page

---
+++ components/__tests__/ResetPassword.test.tsx
@@ -0,0 +1,43 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ResetPassword from '../../pages/sign-up';
+import { populateFormFields } from './__utils__/test-utils';
+
+describe('Reset Password page', () => {
+  it('should validate that passwords are matching', async () => {
+    render(<ResetPassword />);
+    await populateFormFields({
+      Password: 'CorrectHorseBatteryStaple',
+      'Confirm Password': 'CorrectHorseBatteryStaple'
+    });
+    expect(screen.getByLabelText('Confirm Password')).toHaveProperty(
+      'validationMessage',
+      ''
+    );
+  });
+
+  it('should validate that passwords are not matching', async () => {
+    render(<ResetPassword />);
+    await populateFormFields({
+      Password: 'CorrectHorseBatteryStaple',
+      'Confirm Password': 'CorrectHorseBatteryStale'
+    });
+    expect(screen.getByLabelText('Confirm Password')).toHaveProperty(
+      'validationMessage',
+      'Passwords must match'
+    );
+  });
+
+  it('should require all form fields to be populated', async () => {
+    render(<ResetPassword />);
+    const requiredFields = ['Password', 'Confirm Password'];
+    await userEvent.click(screen.getByRole('button', { name: 'Sign Up' }));
+    requiredFields.forEach((labelText) => {
+      expect(screen.getByLabelText(labelText)).toHaveProperty(
+        'validity.valueMissing',
+        true
+      );
+    });
+  });
+});
