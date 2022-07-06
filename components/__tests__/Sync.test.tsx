c48cbd1400547b2d5dcb9d42e6c3751c43d2c4bd
Caleb Evans <caleb@calebevans.me>
2022-07-06 12:46:48 -0700

    WIP: add test for sync push
    It still needs to be worked on, but it is so far, so good.

--- components/__tests__/Sync.test.tsx
+++ components/__tests__/Sync.test.tsx
@@ -61,4 +61,46 @@ describe('Sync functionality', () => {
     userStub.mockRestore();
     jest.useRealTimers();
   });
+  it('should push local dashboard if nothing to pull', async () => {
+    jest.useFakeTimers();
+    const userStub = mockSupabaseUser();
+    const sessionStub = mockSupabaseSession();
+    const supabaseDbStub = mockSupabaseFrom();
+    supabaseFromMocks.dashboards.select.mockImplementation(() => {
+      return { data: [] } as any;
+    });
+    supabaseFromMocks.widgets.select.mockImplementation(() => {
+      return { data: [] } as any;
+    });
+    supabaseFromMocks.dashboards.upsert.mockImplementation(() => {
+      return { data: [] } as any;
+    });
+    supabaseFromMocks.widgets.upsert.mockImplementation(() => {
+      return { data: [] } as any;
+    });
+    assignIdToLocalApp();
+    render(<Home />);
+    expect(
+      screen.getByRole('button', { name: 'Your Account' })
+    ).toBeInTheDocument();
+    await waitFor(() => {
+      expect(supabase.from).toHaveBeenNthCalledWith(1, 'dashboards');
+      expect(supabaseFromMocks.dashboards.select).toHaveBeenCalledTimes(1);
+      expect(supabaseFromMocks.widgets.select).toHaveBeenCalledTimes(0);
+      expect(supabaseFromMocks.dashboards.upsert).toHaveBeenCalledTimes(1);
+      // TODO: the upsert() call referenced below should actually be called 4
+      // times instead of 3; the issue is due to the fact that the Podcast
+      // widget sets isLoading:true immediately by default, yet
+      // pushLocalWidgetToServer() in the useWidgetSync() hook is designed to
+      // skip the push if isLoading is true (so that we don't always push the
+      // widget on initial page load, even if it hasn't changed otherwise)
+      expect(supabaseFromMocks.widgets.upsert).toHaveBeenCalledTimes(3);
+    });
+    supabaseFromMocks.dashboards.select.mockRestore();
+    supabaseFromMocks.widgets.select.mockRestore();
+    supabaseDbStub.mockRestore();
+    sessionStub.mockRestore();
+    userStub.mockRestore();
+    jest.useRealTimers();
+  });
 });
