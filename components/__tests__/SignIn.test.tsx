fd294693dacc9d99ce4dccc2d028250604afa200
Caleb Evans <caleb@calebevans.me>
2022-07-09 12:06:21 -0700

    Temporarily disable CAPTCHA tests
    Per 704a542.

--- components/__tests__/SignIn.test.tsx
+++ components/__tests__/SignIn.test.tsx
@@ -3,10 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SignIn from '../../pages/sign-in';
 import { supabase } from '../supabaseClient';
-import {
-  mockCaptchaFailOnce,
-  mockCaptchaSuccessOnce
-} from './__mocks__/captchaMockUtils';
+import { mockCaptchaSuccessOnce } from './__mocks__/captchaMockUtils';
 import { mockSupabaseApiResponse } from './__mocks__/supabaseMockUtils';
 import { populateFormFields } from './__utils__/testUtils';
 
@@ -38,18 +35,18 @@ describe('Sign In page', () => {
     );
   });
 
-  it('should require CAPTCHA to be completed', async () => {
-    mockCaptchaFailOnce();
-    render(<SignIn />);
-    await populateFormFields({
-      Email: 'notanemail',
-      Password: 'CorrectHorseBatteryStaple'
-    });
-    await userEvent.click(screen.getByRole('button', { name: 'Sign In' }));
-    expect(
-      screen.getByText('Error: Please complete the CAPTCHA')
-    ).toBeInTheDocument();
-  });
+  // it('should require CAPTCHA to be completed', async () => {
+  //   mockCaptchaFailOnce();
+  //   render(<SignIn />);
+  //   await populateFormFields({
+  //     Email: 'notanemail',
+  //     Password: 'CorrectHorseBatteryStaple'
+  //   });
+  //   await userEvent.click(screen.getByRole('button', { name: 'Sign In' }));
+  //   expect(
+  //     screen.getByText('Error: Please complete the CAPTCHA')
+  //   ).toBeInTheDocument();
+  // });
 
   it('should sign in successfully', async () => {
     mockCaptchaSuccessOnce('mytoken');
@@ -73,7 +70,7 @@ describe('Sign In page', () => {
         password: 'CorrectHorseBatteryStaple'
       },
       {
-        captchaToken: 'mytoken'
+        // captchaToken: 'mytoken'
       }
     );
   });
--- components/__tests__/SignUp.test.tsx
+++ components/__tests__/SignUp.test.tsx
@@ -3,10 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SignUp from '../../pages/sign-up';
 import { supabase } from '../supabaseClient';
-import {
-  mockCaptchaFailOnce,
-  mockCaptchaSuccessOnce
-} from './__mocks__/captchaMockUtils';
+import { mockCaptchaSuccessOnce } from './__mocks__/captchaMockUtils';
 import { mockSupabaseApiResponse } from './__mocks__/supabaseMockUtils';
 import { populateFormFields } from './__utils__/testUtils';
 
@@ -98,22 +95,22 @@ describe('Sign Up page', () => {
     );
   });
 
-  it('should require CAPTCHA to be completed', async () => {
-    mockCaptchaFailOnce();
-    render(<SignUp />);
-    await populateFormFields({
-      'First Name': 'John',
-      'Last Name': 'Doe',
-      Email: 'john@example.com',
-      'Confirm Email': 'john@example.com',
-      Password: 'CorrectHorseBatteryStaple',
-      'Confirm Password': 'CorrectHorseBatteryStaple'
-    });
-    await userEvent.click(screen.getByRole('button', { name: 'Sign Up' }));
-    expect(
-      screen.getByText('Error: Please complete the CAPTCHA')
-    ).toBeInTheDocument();
-  });
+  // it('should require CAPTCHA to be completed', async () => {
+  //   mockCaptchaFailOnce();
+  //   render(<SignUp />);
+  //   await populateFormFields({
+  //     'First Name': 'John',
+  //     'Last Name': 'Doe',
+  //     Email: 'john@example.com',
+  //     'Confirm Email': 'john@example.com',
+  //     Password: 'CorrectHorseBatteryStaple',
+  //     'Confirm Password': 'CorrectHorseBatteryStaple'
+  //   });
+  //   await userEvent.click(screen.getByRole('button', { name: 'Sign Up' }));
+  //   expect(
+  //     screen.getByText('Error: Please complete the CAPTCHA')
+  //   ).toBeInTheDocument();
+  // });
 
   it('should create account successfully', async () => {
     mockCaptchaSuccessOnce('mytoken');
@@ -141,7 +138,7 @@ describe('Sign Up page', () => {
         password: 'CorrectHorseBatteryStaple'
       },
       {
-        captchaToken: 'mytoken',
+        // captchaToken: 'mytoken',
         data: {
           first_name: 'John',
           last_name: 'Doe'
