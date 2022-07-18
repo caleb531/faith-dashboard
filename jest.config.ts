c0447c93754e166b149d0613c3ba97ff7054d8e2
Caleb Evans <caleb@calebevans.me>
2022-07-17 19:06:35 -0700

    Exclude UpdateNotification from coverage reporting
    It cannot be easily tested because of the use of service workers and the
    Workbox library.

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,14 @@ const config: Config.InitialOptions = {
   // Do not attempt to test utility functions, since they themselves aren't
   // tests
   testPathIgnorePatterns: ['/__utils__/', '/__mocks__/'],
-  coveragePathIgnorePatterns: ['/__utils__/', '/__mocks__/'],
+  coveragePathIgnorePatterns: [
+    '/__utils__/',
+    '/__mocks__/',
+    // The Update Notification component is difficult to test due to the use of
+    // service workers and the Workbox library, so we exclude it from coverage
+    // reporting
+    'UpdateNotification'
+  ],
   // Do not attempt to transform lodash-es, since it uses native ES6 modules
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
   // Enable Jest to compile TypeScript/JSX using Next's built-in Babel preset
