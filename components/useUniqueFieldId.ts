a801e15c7bf546dea0bb095cffa0f3781f6875ec
Caleb Evans <caleb@calebevans.me>
2022-09-20 12:52:28 -0700

    Use React 18's useId() hook
    The useId() hook was introduced in React 18 and is guaranteed to produce
    field IDs that are unique, stable, and not prone to hydration
    mismatches. For more information, see:
    <https://reactjs.org/docs/hooks-reference.html#useid>

--- components/useUniqueFieldId.ts
+++ components/useUniqueFieldId.ts
@@ -1,11 +1,9 @@
-import { uniqueId } from 'lodash-es';
-import { useState } from 'react';
+import { useId } from 'react';
 
 // The useUniqueFieldId() generates an ID for an HTML <input> in a way that
 // allows it to be reused for a companion <label> element; this ID is
 // guaranteed to be unique across the entire application
 export default function useUniqueFieldId(key: string): string {
-  const [fieldId, setFieldId] = useState(() => uniqueId(`${key}-`));
-
+  const fieldId = `${key}-${useId()}`;
   return fieldId;
 }
