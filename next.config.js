caleb531/faith-dashboard

commit 3bb979d47a0f8f7e65ab2d887301d1b2eed52133

Attempt to fix analytics CSP violation
To accomplish this, we've done a few things:
- We replaced GTM with GA4
- We are serving all pages server-side now so we can expose nonces to
  the front end

We'll see how this works.

next.config.js
@@ -13,11 +13,6 @@ const nextConfig = withPWA({
       {
         key: 'X-Content-Type-Options',
         value: 'nosniff'
-      },
-      {
-        key: 'Content-Security-Policy',
-        value:
-          "default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com; font-src 'self' https://fonts.gstatic.com data:; img-src * data:; script-src 'self' https://storage.googleapis.com www.googletagmanager.com https://hcaptcha.com https://*.hcaptcha.com; child-src 'self' https://hcaptcha.com https://*.hcaptcha.com; prefetch-src 'self'; connect-src *; manifest-src 'self'; media-src *;"
       }
     ];
     // The HSTS header should only be sent for HTTPS websites; because localhost is server over plain HTTP, we do not want to enable HSTS there

pages/_app.tsx
@@ -1,21 +1,10 @@
 import { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
 import { PageProps } from '../components/global';
 import PageHead from '../components/PageHead';
 import '../styles/index.scss';
 import '../styles/landing-page.scss';
 
 function AppWrapper({ Component, pageProps }: AppProps<PageProps>) {
-  // Initialize Google Tag Manager when page is mounted
-  useEffect(() => {
-    if (
-      typeof window !== 'undefined' &&
-      process.env.NODE_ENV === 'production'
-    ) {
-      TagManager.initialize({ gtmId: 'GTM-M3QKM2Z' });
-    }
-  }, []);
   return (
     <>
       <PageHead {...pageProps} />

pages/_document.tsx
@@ -1,10 +1,25 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import React from 'react';
+import useGoogleAnalytics from '../components/useGoogleAnalytics';
+
+// Generate a nonce for use by the CSP
+function generateNonce() {
+  return Math.round(Math.random() * 1e16).toString(16);
+}
 
 function AppDocument() {
+  const ga4Nonce = generateNonce();
+  useGoogleAnalytics('G-QDGNKNKW4E', {
+    nonce: ga4Nonce
+  });
   return (
     <Html lang="en">
       <Head>
+        {process.env.NODE_ENV === 'production' ? (
+          <meta
+            httpEquiv="Content-Security-Policy"
+            content={`default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com; font-src 'self' https://fonts.gstatic.com data:; img-src * data:; script-src 'self' 'nonce-${ga4Nonce}' https://storage.googleapis.com www.googletagmanager.com https://hcaptcha.com https://*.hcaptcha.com; child-src 'self' https://hcaptcha.com https://*.hcaptcha.com; prefetch-src 'self'; connect-src *; manifest-src 'self'; media-src *;`}
+          />
+        ) : null}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"

components/useGoogleAnalytics.ts
@@ -1,0 +1,29 @@
+import { useEffect } from 'react';
+import ReactGA4 from 'react-ga4';
+
+type Options = Parameters<typeof ReactGA4.initialize>[1];
+
+// The useGoogleAnalytics() hook is a simple abstraction around the react-ga
+// library for loading and firing Google Analytics
+function useGoogleAnalytics(trackingId: string, options: Options = {}) {
+  // Initialize Google Analytics on the initial page load
+  useEffect(() => {
+    if (
+      typeof window !== 'undefined' &&
+      process.env.NODE_ENV === 'production'
+    ) {
+      ReactGA4.initialize(trackingId, options);
+    }
+  }, [trackingId, options]);
+  // Trigger a pageview even on the initial page load and when navigating to
+  // another page on the site
+  useEffect(() => {
+    if (
+      typeof window !== 'undefined' &&
+      process.env.NODE_ENV === 'production'
+    ) {
+      ReactGA4.send('pageview');
+    }
+  });
+}
+export default useGoogleAnalytics;

pages/account.tsx
@@ -223,7 +223,7 @@ function AccountSettings() {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   return {
     props: {
       pageTitle: 'Account Settings | Faith Dashboard',

pages/copyright.tsx
@@ -26,7 +26,7 @@ function Copyright() {
 }
 
 /* istanbul ignore next */
-export async function getStaticProps() {
+export async function getServerSideProps() {
   return {
     props: {
       pagePath: '/copyright',

pages/forgot-password.tsx
@@ -40,7 +40,7 @@ function ForgotPassword() {
 }
 
 /* istanbul ignore next */
-export async function getStaticProps() {
+export async function getServerSideProps() {
   return {
     props: {
       pagePath: '/forgot-password',

pages/help.tsx
@@ -173,7 +173,7 @@ function Help() {
 }
 
 /* istanbul ignore next */
-export async function getStaticProps() {
+export async function getServerSideProps() {
   return {
     props: {
       pagePath: '/help',

commit c6b6eb352ebf26d788eb1b1f25260b9d8cd90f89

Fix Google Analytics not loading on front end
This bug was ultimately caused by the refactor in 3bb979d, because
_document.tsx is only rendered on the server-side (meaning a useEffect
hook inside _document will never actually run). But per my testing on my
personal website, it seems we may not need a nonce anyway. Trying
without it to see where we get.

TODO: if we can do without the nonce, make sure to switch back to
getStaticProps to enable static generation / disable server-side
rendering.

next.config.js
@@ -15,11 +15,21 @@ const nextConfig = withPWA({
         value: 'nosniff'
       }
     ];
-    // The HSTS header should only be sent for HTTPS websites; because localhost is server over plain HTTP, we do not want to enable HSTS there
     if (process.env.NODE_ENV === 'production') {
+      // The HSTS header should only be sent for HTTPS websites; because
+      // localhost is server over plain HTTP, we do not want to enable HSTS
+      // there
       headers.push({
         key: 'Strict-Transport-Security',
         value: 'max-age=15552000; includeSubDomains'
+      });
+      // Only apply Content Security Policy for production build
+      headers.push({
+        key: 'Content-Security-Policy',
+        /* eslint-disable quotes */
+        value:
+          "default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com; font-src 'self' https://fonts.gstatic.com data:; img-src * data:; script-src 'self' https://storage.googleapis.com www.googletagmanager.com https://hcaptcha.com https://*.hcaptcha.com; child-src 'self' https://hcaptcha.com https://*.hcaptcha.com; prefetch-src 'self'; connect-src *; manifest-src 'self'; media-src *;"
+        /* eslint-enable quotes */
       });
     }
     return [{ source: '/:path*', headers }];

pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from 'next/app';
 import { PageProps } from '../components/global';
 import PageHead from '../components/PageHead';
+import useGoogleAnalytics from '../components/useGoogleAnalytics';
 import '../styles/index.scss';
 import '../styles/landing-page.scss';
 
 function AppWrapper({ Component, pageProps }: AppProps<PageProps>) {
+  useGoogleAnalytics('G-QDGNKNKW4E');
   return (
     <>
       <PageHead {...pageProps} />

pages/_document.tsx
@@ -1,25 +1,9 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import useGoogleAnalytics from '../components/useGoogleAnalytics';
-
-// Generate a nonce for use by the CSP
-function generateNonce() {
-  return Math.round(Math.random() * 1e16).toString(16);
-}
 
 function AppDocument() {
-  const ga4Nonce = generateNonce();
-  useGoogleAnalytics('G-QDGNKNKW4E', {
-    nonce: ga4Nonce
-  });
   return (
     <Html lang="en">
       <Head>
-        {process.env.NODE_ENV === 'production' ? (
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content={`default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com; font-src 'self' https://fonts.gstatic.com data:; img-src * data:; script-src 'self' 'nonce-${ga4Nonce}' https://storage.googleapis.com www.googletagmanager.com https://hcaptcha.com https://*.hcaptcha.com; child-src 'self' https://hcaptcha.com https://*.hcaptcha.com; prefetch-src 'self'; connect-src *; manifest-src 'self'; media-src *;`}
-          />
-        ) : null}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"