56f20681aa91afb21372307448aff378428e99ea
Caleb Evans <caleb@calebevans.me>
2023-08-09 16:57:09 -0700

    Fix comments in useMultipleDashboardsMigration() hook

--- components/useMultipleDashboardsMigration.ts
+++ components/useMultipleDashboardsMigration.ts
@@ -1,15 +1,16 @@
-// The useMultipleDashboardsMigration() hook migrates the app's local data store
-
 import { AppState } from './app/app.d';
 import { getSelectedAppIdStorageKey } from './storageUtils';
 import useLocalStorage from './useLocalStorage';
 
+// The useMultipleDashboardsMigration() hook migrates the app's local data store
 // to a version that supports the multiple dashboard functionality
 function useMultipleDashboardsMigration(app: AppState): void {
   const [getSelectedAppId, setSelectedAppId] = useLocalStorage<string | null>(
     getSelectedAppIdStorageKey(),
     null
   );
+  // Always ensure that the selected app ID in the local store is always in sync
+  // with the ID of the locally-persisted app/dashboard
   if (getSelectedAppId() !== app.id && app.id) {
     setSelectedAppId(app.id);
   }
