a07aa5753e59f5021812f1dd958ea43c2087d365
Caleb Evans <caleb@calebevans.me>
2023-08-12 12:06:06 -0700

    Make useAllSearchParams() hook isomorphic

--- components/useAllSearchParams.ts
+++ components/useAllSearchParams.ts
@@ -3,6 +3,9 @@ import { fromPairs } from 'lodash-es';
 // The useAllSearchParams() hook retrieves an object of all search parameters
 // for the current URL;
 function useAllSearchParams() {
+  if (typeof window === 'undefined') {
+    return {};
+  }
   const url = new URL(window.location.href);
   return fromPairs(Array.from(url.searchParams.entries()));
 }
