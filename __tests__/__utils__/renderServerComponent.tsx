f59be1fac42cff494db45499f0b9baef74a91c0d
Caleb Evans <caleb@calebevans.me>
2023-08-12 16:49:07 -0700

    Add comment for renderServerComponent()
    Also remove leftover variable.

--- __tests__/__utils__/renderServerComponent.tsx
+++ __tests__/__utils__/renderServerComponent.tsx
@@ -1,7 +1,8 @@
 import { act, render } from '@testing-library/react';
 
-const fallbackText = 'Loading test...';
-
+// Follow <https://github.com/testing-library/react-testing-library/issues/1209>
+// for the latest updates on React Testing Library support for React Server
+// Components (RSC)
 export async function renderServerComponent(Element: JSX.Element) {
   await act(async () => {
     render(await Element.type({ ...Element.props }));
