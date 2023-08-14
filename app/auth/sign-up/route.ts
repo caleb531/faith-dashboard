b982858f435db2c471c09b628e8c2d7790cba224
Caleb Evans <caleb@calebevans.me>
2023-08-14 15:36:46 -0700

    Redirect to homepage on successful signup

--- app/auth/sign-up/route.ts
+++ app/auth/sign-up/route.ts
@@ -36,11 +36,8 @@ export async function POST(request: Request) {
     );
   }
 
-  return NextResponse.redirect(
-    `${requestUrl.origin}/login?message=Check email to continue sign in process`,
-    {
-      // a 301 status is required to redirect from a POST to a GET route
-      status: 301
-    }
-  );
+  return NextResponse.redirect(requestUrl.origin, {
+    // a 301 status is required to redirect from a POST to a GET route
+    status: 301
+  });
 }
