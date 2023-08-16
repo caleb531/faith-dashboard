f1116d03e10bca7985ea0886bae8febde12af620
Caleb Evans <caleb@calebevans.me>
2023-08-15 17:18:24 -0700

    Rename Element parameter to node
    Since it is actually a React node, and cannot be used as a JSX tag name.

--- __tests__/__utils__/renderServerComponent.tsx
+++ __tests__/__utils__/renderServerComponent.tsx
@@ -3,8 +3,8 @@ import { act, render } from '@testing-library/react';
 // Follow <https://github.com/testing-library/react-testing-library/issues/1209>
 // for the latest updates on React Testing Library support for React Server
 // Components (RSC)
-export async function renderServerComponent(Element: JSX.Element) {
+export async function renderServerComponent(node: JSX.Element) {
   await act(async () => {
-    render(await Element.type({ ...Element.props }));
+    render(await node.type({ ...node.props }));
   });
 }
