6d9a084d30b0a3bedf447ac983538eb61c4adc92
Caleb Evans <caleb@calebevans.me>
2023-08-17 16:18:54 -0700

    Remove obsolete getter/setters for redirectTo parameter
    We can just access the parameter directly from useAllSearchParams(), as
    we are currently doing.

--- components/account/SignInForm.tsx
+++ components/account/SignInForm.tsx
@@ -2,15 +2,10 @@
 import AuthForm, { redirectToHome } from '@components/account/AuthForm';
 import AuthFormField from '@components/account/AuthFormField';
 import useAutoFocus from '@components/account/useAutoFocus';
-import { useState } from 'react';
 import useAllSearchParams from '../useAllSearchParams';
 
 function SignInForm() {
   const params = useAllSearchParams();
-  // We need to use component state (and useEffect below) to set the redirectTo
-  // variable in order to ensure that the SSR output matches the client-side
-  // output
-  const [redirectTo, setRedirectTo] = useState('');
   const emailAutoFocusProps = useAutoFocus<HTMLInputElement>();
 
   function redirectToDestination() {
