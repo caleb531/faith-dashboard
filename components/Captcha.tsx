8817812d708a36f93e6c0759b717f188ef729293
Caleb Evans <caleb@calebevans.me>
2023-08-18 16:11:08 -0700

    Memoize Captcha component

--- components/Captcha.tsx
+++ components/Captcha.tsx
@@ -1,10 +1,13 @@
 import { Turnstile } from '@marsidev/react-turnstile';
+import React from 'react';
 
 type Props = {
   setCaptchaToken: (token: string) => void;
 };
 
-function Captcha({ setCaptchaToken }: Props) {
+// Memoize Captcha component to ensure that Turnstile script is not loaded more
+// than once
+const Captcha = React.memo(function Captcha({ setCaptchaToken }: Props) {
   const siteKey = process.env.NEXT_PUBLIC_GOTRUE_SECURITY_CAPTCHA_SITEKEY;
 
   return siteKey ? (
@@ -21,6 +24,6 @@ function Captcha({ setCaptchaToken }: Props) {
       options={{ size: 'invisible' }}
     />
   ) : null;
-}
+});
 
 export default Captcha;
