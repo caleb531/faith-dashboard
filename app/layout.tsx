d5d64dd665c31ba76585b1effec89488b68971c3
Caleb Evans <caleb@calebevans.me>
2023-08-19 12:04:38 -0700

    Revamp fonts
    - Merriweather Sans has been replaced with Lato for a cleaner and more
      friendly feel
    - The type scale has been rewritten to eliminate Sass and rely solely on
      rem units, CSS custom properties, and calc()
    - Additional font variables have been added for line-height and
      font-weight for better semantics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import '@fontsource/merriweather-sans/300.css';
-import '@fontsource/merriweather-sans/400.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700-italic.css';
+import '@fontsource/lato/700.css';
 import '@fontsource/merriweather/300-italic.css';
 import '@fontsource/merriweather/300.css';
 import '@fontsource/merriweather/400-italic.css';
