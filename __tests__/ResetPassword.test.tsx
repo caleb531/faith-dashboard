7bc054d5ed9470560c7105ca95cf32041564377a
Caleb Evans <caleb@calebevans.me>
2023-08-20 10:02:23 -0700

    Fix tests for Reset Password
    They were pointing to the wrong page component (SignUp instead of
    ResetPassword).

--- __tests__/ResetPassword.test.tsx
+++ __tests__/ResetPassword.test.tsx
@@ -1,39 +1,51 @@
-import ResetPassword from '@app/sign-up/page';
+import ResetPassword from '@app/reset-password/page';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderServerComponent } from '@tests/__utils__/renderServerComponent';
 import { populateFormFields } from '@tests/__utils__/testUtils';
+import {
+  mockSupabaseSession,
+  mockSupabaseUser
+} from './__utils__/supabaseMockUtils';
 
 describe('Reset Password page', () => {
   it('should validate that passwords are matching', async () => {
+    await mockSupabaseUser();
+    await mockSupabaseSession();
     await renderServerComponent(<ResetPassword />);
     await populateFormFields({
-      Password: 'CorrectHorseBatteryStaple',
-      'Confirm Password': 'CorrectHorseBatteryStaple'
+      'New Password': 'CorrectHorseBatteryStaple',
+      'Confirm New Password': 'CorrectHorseBatteryStaple'
     });
-    expect(screen.getByLabelText('Confirm Password')).toHaveProperty(
+    expect(screen.getByLabelText('Confirm New Password')).toHaveProperty(
       'validationMessage',
       ''
     );
   });
 
   it('should validate that passwords are not matching', async () => {
+    await mockSupabaseUser();
+    await mockSupabaseSession();
     await renderServerComponent(<ResetPassword />);
     await populateFormFields({
-      Password: 'CorrectHorseBatteryStaple',
-      'Confirm Password': 'CorrectHorseBatteryStale'
+      'New Password': 'CorrectHorseBatteryStaple',
+      'Confirm New Password': 'CorrectHorseBatteryStale'
     });
-    expect(screen.getByLabelText('Confirm Password')).toHaveProperty(
+    expect(screen.getByLabelText('Confirm New Password')).toHaveProperty(
       'validationMessage',
       'Passwords must match'
     );
   });
 
   it('should require all form fields to be populated', async () => {
+    await mockSupabaseUser();
+    await mockSupabaseSession();
     await renderServerComponent(<ResetPassword />);
-    const requiredFields = ['Password', 'Confirm Password'];
-    await userEvent.click(screen.getByRole('button', { name: 'Sign Up' }));
+    const requiredFields = ['New Password', 'Confirm New Password'];
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Reset Password' })
+    );
     requiredFields.forEach((labelText) => {
       expect(screen.getByLabelText(labelText)).toHaveProperty(
         'validity.valueMissing',
