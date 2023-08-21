d792a40ac8cc990b03fd0d67ed93964a03817289
Caleb Evans <caleb@calebevans.me>
2023-08-20 18:02:45 -0700

    Add test for dismissing app notifications

--- __tests__/AppNotification.test.tsx
+++ __tests__/AppNotification.test.tsx
@@ -1,6 +1,7 @@
 import Home from '@app/page';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderServerComponent } from '@tests/__utils__/renderServerComponent';
 
 describe('App Notification', () => {
@@ -9,9 +10,19 @@ describe('App Notification', () => {
     await renderServerComponent(<Home />);
     expect(screen.getByText('This is my message')).toBeInTheDocument();
   });
+
   it('should display the specified error message from the URL hash', async () => {
     window.location.hash = '#error_description=There+was+an+error';
     await renderServerComponent(<Home />);
     expect(screen.getByText('There was an error')).toBeInTheDocument();
+  });
+
+  it('should be dismissable via the modal Close button', async () => {
+    window.location.hash = '#message=This+is+my+message';
+    await renderServerComponent(<Home />);
+    expect(screen.getByText('This is my message')).toBeInTheDocument();
+    await userEvent.click(screen.getByRole('button', { name: 'Close Modal' }));
+    expect(window.location.hash).toEqual('');
+    expect(screen.queryByText('This is my message')).not.toBeInTheDocument();
   });
 });
