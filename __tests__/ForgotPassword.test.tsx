e9d57f7bd11491ebdc88759f66904d642bfd26fe
Caleb Evans <caleb@calebevans.me>
2023-08-21 15:45:46 -0700

    Add test for /request-password-reset auth route

--- __tests__/ForgotPassword.test.tsx
+++ __tests__/ForgotPassword.test.tsx
@@ -1,10 +1,15 @@
+import { POST as RequestPasswordResetPOST } from '@app/auth/request-password-reset/route';
 import ForgotPassword from '@app/forgot-password/page';
 import SignIn from '@app/sign-in/page';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderServerComponent } from '@tests/__utils__/renderServerComponent';
-import { typeIntoFormFields } from '@tests/__utils__/testUtils';
+import {
+  callRouteHandler,
+  typeIntoFormFields
+} from '@tests/__utils__/testUtils';
+import { supabase } from './__mocks__/supabaseAuthHelpersMock';
 
 describe('Forgot Password page', () => {
   it('should be accessible from Sign In page', async () => {
@@ -31,6 +36,30 @@ describe('Forgot Password page', () => {
     expect(screen.getByLabelText('Email')).toHaveProperty(
       'validity.typeMismatch',
       true
+    );
+  });
+
+  it('should call Supabase API correctly on server', async () => {
+    jest
+      .spyOn(supabase.auth, 'resetPasswordForEmail')
+      .mockImplementationOnce(async () => {
+        return { data: { user: {}, session: {} }, error: null } as any;
+      });
+    const fields = {
+      email: 'caleb@calebevans.me',
+      'cf-turnstile-response': 'abc123'
+    };
+    await callRouteHandler({
+      handler: RequestPasswordResetPOST,
+      path: '/auth/request-password-reset',
+      fields
+    });
+    expect(supabase.auth.resetPasswordForEmail).toHaveBeenCalledWith(
+      fields.email,
+      {
+        captchaToken: fields['cf-turnstile-response'],
+        redirectTo: 'http://localhost:3000/reset-password'
+      }
     );
   });
 });
