797252e7f336fa96744d5332cd49f4c1c74b3958
Caleb Evans <caleb@calebevans.me>
2023-08-21 13:26:19 -0700

    Abstract out generic logic from server auth test
    This will make it easier to write the additional server auth tests.

--- __tests__/ServerAuth.test.tsx
+++ __tests__/ServerAuth.test.tsx
@@ -1,7 +1,28 @@
 import { POST as SignInPOST } from '@app/auth/sign-in/route';
 import { convertObjectToFormData } from '@components/authUtils.client';
 import { supabase } from '@tests/__mocks__/supabaseAuthHelpersMock';
+import { NextResponse } from 'next/server';
 import { NextRequest } from './__mocks__/nextServer';
+
+type HandlerType = (req: NextRequest) => Promise<NextResponse>;
+
+async function postToRouteHandler({
+  handler,
+  path,
+  fields
+}: {
+  handler: HandlerType;
+  path: string;
+  fields: object;
+}) {
+  NextRequest._formData = convertObjectToFormData(fields);
+  return handler(
+    new NextRequest('http://localhost:3000/auth/sign-in', {
+      method: 'POST',
+      body: NextRequest._formData
+    })
+  );
+}
 
 describe('server routes', () => {
   it('should include sign in functionality', async () => {
@@ -10,22 +31,21 @@ describe('server routes', () => {
       .mockImplementationOnce(async () => {
         return { data: { user: {}, session: {} }, error: null } as any;
       });
-    NextRequest._formData = convertObjectToFormData({
+    const fields = {
       email: 'caleb@calebevans.me',
       password: 'CorrectHorseBatteryStaple',
       'cf-turnstile-response': 'abc123'
+    };
+    await postToRouteHandler({
+      handler: SignInPOST,
+      path: '/auth/sign-in',
+      fields
     });
-    await SignInPOST(
-      new NextRequest('http://localhost:3000/auth/sign-in', {
-        method: 'POST',
-        body: NextRequest._formData
-      })
-    );
     expect(supabase.auth.signInWithPassword).toHaveBeenCalledWith({
-      email: 'caleb@calebevans.me',
-      password: 'CorrectHorseBatteryStaple',
+      email: fields.email,
+      password: fields.password,
       options: {
-        captchaToken: 'abc123'
+        captchaToken: fields['cf-turnstile-response']
       }
     });
   });
