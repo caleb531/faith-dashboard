3cfb7b9e7230070a6ad79590f3ac577b29b82c12
Caleb Evans <caleb@calebevans.me>
2023-08-28 18:01:36 -0700

    Consolidate code to test Dashboard Manager

--- __tests__/DashboardManager.test.tsx
+++ __tests__/DashboardManager.test.tsx
@@ -1,4 +1,6 @@
 import Home from '@app/page';
+import { AppState } from '@components/app/app.types';
+import photoThemeList from '@components/app/appPhotoThemeList';
 import widgetSyncService from '@components/widgets/widgetSyncService';
 import '@testing-library/jest-dom';
 import { act, fireEvent, screen, waitFor } from '@testing-library/react';
@@ -15,6 +17,64 @@ import {
   supabaseFromMocks
 } from '@tests/__utils__/supabaseMockUtils';
 import { setAppData } from '@tests/__utils__/testUtils';
+import { capitalize } from 'lodash-es';
+
+async function openDashboardManager({
+  localDashboard,
+  availableDashboards
+}: {
+  localDashboard: AppState;
+  availableDashboards: AppState[];
+}): Promise<void> {
+  await mockSupabaseUser();
+  await mockSupabaseSession();
+  mockSupabaseFrom();
+  mockSupabaseSelectOnce('dashboards', {
+    data: [{ raw_data: secondDashboardJson }]
+  });
+  mockSupabaseSelectOnce('dashboards', {
+    data: availableDashboards.map((dashboard) => {
+      return { raw_data: dashboard };
+    })
+  });
+  mockSupabaseSelect('widgets', { data: [] });
+  setAppData(firstDashboardJson);
+  await renderServerComponent(<Home />);
+  await waitFor(() => {
+    expect(
+      screen.getByRole('button', { name: 'Your Account' })
+    ).toBeInTheDocument();
+  });
+  await waitFor(() => {
+    expect(supabaseFromMocks.dashboards.select).toHaveBeenCalledTimes(1);
+    expect(supabaseFromMocks.widgets.select).toHaveBeenCalled();
+  });
+  // Reset the calls and call counts on the supabase mocks
+  supabaseFromMocks.dashboards.select.mockClear();
+  // For some reason, using userEvent.click() for the below causes Jest to
+  // timeout; so we are using fireEvent instead
+  fireEvent.click(screen.getByRole('button', { name: 'Your Account' }));
+  fireEvent.click(screen.getByRole('link', { name: 'My Dashboards' }));
+  expect(
+    screen.getByRole('heading', { name: 'My Dashboards' })
+  ).toBeInTheDocument();
+  await waitFor(() => {
+    availableDashboards.forEach((dashboard) => {
+      expect(screen.getByText(String(dashboard.name))).toBeInTheDocument();
+    });
+  });
+  // Ensure that current dashboard hasn't changed (mostly as a sanity check)
+  expect(
+    screen.getByText(
+      String(
+        photoThemeList.find((theme) => {
+          return theme.id === localDashboard.theme;
+        })?.name || capitalize(localDashboard.theme)
+      )
+    )
+  ).toBeInTheDocument();
+  expect(screen.queryByText('Shore')).not.toBeInTheDocument();
+}
 
 const originalOnPush = widgetSyncService.onPush;
 const originalBroadcastPush = widgetSyncService.broadcastPush;
@@ -32,93 +92,25 @@ describe('Dashboard Manager', () => {
   });
 
   it('should open and fetch all user dashboards', async () => {
-    await mockSupabaseUser();
-    await mockSupabaseSession();
-    mockSupabaseFrom();
-    mockSupabaseSelectOnce('dashboards', {
-      data: [{ raw_data: secondDashboardJson }]
-    });
-    mockSupabaseSelectOnce('dashboards', {
-      data: [
-        { raw_data: firstDashboardJson },
-        { raw_data: secondDashboardJson },
-        { raw_data: thirdDashboardJson }
+    await openDashboardManager({
+      localDashboard: secondDashboardJson as AppState,
+      availableDashboards: [
+        firstDashboardJson as AppState,
+        secondDashboardJson as AppState,
+        thirdDashboardJson as AppState
       ]
     });
-    mockSupabaseSelect('widgets', { data: [] });
-    setAppData(firstDashboardJson);
-    await renderServerComponent(<Home />);
-    await waitFor(() => {
-      expect(
-        screen.getByRole('button', { name: 'Your Account' })
-      ).toBeInTheDocument();
-    });
-    await waitFor(() => {
-      expect(supabaseFromMocks.dashboards.select).toHaveBeenCalledTimes(1);
-      expect(supabaseFromMocks.widgets.select).toHaveBeenCalled();
-    });
-    // Reset the calls and call counts on the supabase mocks
-    supabaseFromMocks.dashboards.select.mockClear();
-    // For some reason, using userEvent.click() for the below causes Jest to
-    // timeout; so we are using fireEvent instead
-    fireEvent.click(screen.getByRole('button', { name: 'Your Account' }));
-    fireEvent.click(screen.getByRole('link', { name: 'My Dashboards' }));
-    expect(
-      screen.getByRole('heading', { name: 'My Dashboards' })
-    ).toBeInTheDocument();
-    await waitFor(() => {
-      expect(screen.getByText('Main Dashboard')).toBeInTheDocument();
-      expect(screen.getByText('Mountain Dashboard')).toBeInTheDocument();
-      expect(screen.getByText('Stars Dashboard')).toBeInTheDocument();
-    });
-    // Ensure that current dashboard hasn't changed (mostly as a sanity check)
-    expect(screen.getByText('Mountains')).toBeInTheDocument();
-    expect(screen.queryByText('Shore')).not.toBeInTheDocument();
   });
 
   it('should switch to another dashboard', async () => {
-    await mockSupabaseUser();
-    await mockSupabaseSession();
-    mockSupabaseFrom();
-    mockSupabaseSelectOnce('dashboards', {
-      data: [{ raw_data: firstDashboardJson }]
-    });
-    mockSupabaseSelectOnce('dashboards', {
-      data: [
-        { raw_data: firstDashboardJson },
-        { raw_data: secondDashboardJson },
-        { raw_data: thirdDashboardJson }
+    await openDashboardManager({
+      localDashboard: secondDashboardJson as AppState,
+      availableDashboards: [
+        firstDashboardJson as AppState,
+        secondDashboardJson as AppState,
+        thirdDashboardJson as AppState
       ]
     });
-    mockSupabaseSelect('widgets', { data: [] });
-    setAppData(firstDashboardJson);
-    await renderServerComponent(<Home />);
-    await waitFor(() => {
-      expect(
-        screen.getByRole('button', { name: 'Your Account' })
-      ).toBeInTheDocument();
-    });
-    await waitFor(() => {
-      expect(supabaseFromMocks.dashboards.select).toHaveBeenCalledTimes(1);
-      expect(supabaseFromMocks.widgets.select).toHaveBeenCalled();
-    });
-    // Reset the calls and call counts on the supabase mocks
-    supabaseFromMocks.dashboards.select.mockClear();
-    // For some reason, using userEvent.click() for the below causes Jest to
-    // timeout; so we are using fireEvent instead
-    fireEvent.click(screen.getByRole('button', { name: 'Your Account' }));
-    fireEvent.click(screen.getByRole('link', { name: 'My Dashboards' }));
-    expect(
-      screen.getByRole('heading', { name: 'My Dashboards' })
-    ).toBeInTheDocument();
-    await waitFor(() => {
-      expect(screen.getByText('Main Dashboard')).toBeInTheDocument();
-      expect(screen.getByText('Mountain Dashboard')).toBeInTheDocument();
-      expect(screen.getByText('Stars Dashboard')).toBeInTheDocument();
-    });
-    // Ensure that current dashboard hasn't changed (mostly as a sanity check)
-    expect(screen.getByText('Forest')).toBeInTheDocument();
-    expect(screen.queryByText('Shore')).not.toBeInTheDocument();
     mockSupabaseSelectOnce('dashboards', {
       data: [{ raw_data: thirdDashboardJson }]
     });
