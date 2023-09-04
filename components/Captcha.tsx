fb618f0b3a7651adabe14733fae5b92b350d267b
Caleb Evans <caleb@calebevans.me>
2023-09-04 11:52:25 -0700

    Update React turnstile package
    It looks like the latest (v0.3.0) includes a fix for the warning about
    duplicate Turnstile instances:
    <https://github.com/marsidev/react-turnstile/releases/tag/v0.3.0>

--- components/Captcha.tsx
+++ components/Captcha.tsx
@@ -11,11 +11,6 @@ const Captcha = React.memo(function Captcha({ setCaptchaToken }: Props) {
   const siteKey = process.env.NEXT_PUBLIC_GOTRUE_SECURITY_CAPTCHA_SITEKEY;
 
   return siteKey ? (
-    // TODO: There is a problem with the Turnstile <script> loading multiple
-    // times due to multiple re-renders; it is also loading quite late, since we
-    // have to wait for React to load before we can make the request; consider
-    // adopting the vanilla Turnstile implementation instead using Next's
-    // <Script /> component in layout.tsx
     <Turnstile
       siteKey={siteKey}
       onSuccess={(token) => setCaptchaToken(token)}
