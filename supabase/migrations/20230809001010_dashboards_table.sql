866885ddf0226b864dbe925fc96d6d826c56016f
Caleb Evans <caleb@calebevans.me>
2023-09-07 19:56:35 -0700

    Move initial database schema setup to migration scripts
    Including all table and function creation. This commit also fixes some
    of the SQL syntax and makes the table/policy creation scripts (for
    dashboards and widgets) fully idempotent. This will be important for the
    eventual deploy to Production because the dashboards and widgets tables
    already exists in Production.
    
    Also, it's important to note that the 'dashboards'/'widgets' table
    migrations have been manually inserted before all other migrations (this
    is why the migration version numbers are so exact). To ensure that the
    Staging and Production environments don't have a problem with this,
    we've added the --include-all flag when pushing the database to both
    Supabase environments (see the relevant GitHub actions in the .github/
    directory).

--- supabase/migrations/20230809001010_dashboards_table.sql
+++ supabase/migrations/20230809001010_dashboards_table.sql
@@ -1,7 +1,7 @@
-create table public.dashboards (
+create table if not exists public.dashboards (
   id uuid not null unique,
   raw_data json not null,
-  user_id uuid references auth.users not null on delete cascade,
+  user_id uuid references auth.users on delete cascade not null,
   client_id uuid,
   updated_at timestamptz,
   primary key(id, user_id)
@@ -9,18 +9,22 @@ create table public.dashboards (
 
 alter table public.dashboards enable row level security;
 
+drop policy if exists "Only the user owning the dashboard can view it." on public.dashboards;
 create policy "Only the user owning the dashboard can view it."
   on public.dashboards for select
   using (auth.uid() = user_id);
 
+drop policy if exists "Users can insert their own dashboard." on public.dashboards;
 create policy "Users can insert their own dashboard."
   on public.dashboards for insert
   with check (auth.uid() = user_id);
 
+drop policy if exists "Users can update their own dashboard." on public.dashboards;
 create policy "Users can update their own dashboard."
   on public.dashboards for update
   using (auth.uid() = user_id);
 
+drop policy if exists "Users can delete their own dashboard." on public.dashboards;
 create policy "Users can delete their own dashboard."
   on public.dashboards for delete
   using (auth.uid() = user_id);
--- supabase/migrations/20230809001020_widgets_table.sql
+++ supabase/migrations/20230809001020_widgets_table.sql
@@ -1,27 +1,31 @@
-create table public.widgets (
+create table if not exists public.widgets (
   id uuid not null,
   raw_data json not null,
   user_id uuid references auth.users not null,
   client_id uuid,
   updated_at timestamptz,
-  dashboard_id uuid references public.dashboards(id) not null on delete cascade,
+  dashboard_id uuid references public.dashboards(id) on delete cascade not null,
   primary key(id, user_id, dashboard_id)
 );
 
 alter table public.widgets enable row level security;
 
+drop policy if exists "Only the user owning the widget can view it." on public.widgets;
 create policy "Only the user owning the widget can view it."
   on public.widgets for select
   using (auth.uid() = user_id);
 
+drop policy if exists "Users can insert their own widget." on public.widgets;
 create policy "Users can insert their own widget."
   on public.widgets for insert
   with check (auth.uid() = user_id);
 
+drop policy if exists "Users can update their own widget." on public.widgets;
 create policy "Users can update their own widget."
   on public.widgets for update
   using (auth.uid() = user_id);
 
+drop policy if exists "Users can delete their own widget." on public.widgets;
 create policy "Users can delete their own widget."
   on public.widgets for delete
   using (auth.uid() = user_id);
