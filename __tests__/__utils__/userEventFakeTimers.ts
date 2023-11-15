fddcdd80ed6a5dab489b136453e64bebd3c67bd2
Caleb Evans <caleb@calebevans.me>
2023-11-14 20:39:26 -0800

    Remove obsolete userEventFakeTimers helper
    It seems I no longer need it since the last occurrences were removed in
    221e597.

--- __tests__/__utils__/userEventFakeTimers.ts
+++
@@ -1,9 +0,0 @@
-import userEvent from '@testing-library/user-event';
-
-// Wrap the userEvent library to auto-advance fake timers; this only works when
-// fake timers are currently enabled, otherwise you will receive a warning
-// (source:
-// <https://stackoverflow.com/questions/71901237/fake-timers-doesnt-work-with-latest-version-of-user-event>)
-export default userEvent.setup({
-  advanceTimers: () => vi.runOnlyPendingTimers()
-});
