209bd33dec7cc4532790f01a3ebc753427e9b3aa
Caleb Evans <caleb@calebevans.me>
2026-08-01 19:11:30 -0700

    Fix "Cannot access refs during render" error

--- components/useObjectHasChanged.ts
+++ components/useObjectHasChanged.ts
@@ -1,5 +1,5 @@
 import { diff } from 'deep-object-diff';
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 // The useObjectHasChanged() hook returns true if an object has changed
 // compared to its previous state; otherwise, it returns false
@@ -7,7 +7,9 @@ function useObjectHasChanged<TObject extends object>(obj: TObject) {
   // Keep a ref to the current state of the object, which will always be kept
   // up-to-date
   const currentObjRef = useRef(obj);
-  currentObjRef.current = obj;
+  useEffect(() => {
+    currentObjRef.current = obj;
+  });
   // Keep a ref of the previous state of the object, which will only be updated
   // if the object state has changed (using deep comparison; see below)
   const prevObjRef = useRef(obj);
